b132aa228a3f12742a2ccfac45c781f84b76df30
Mac Mini <macmini@Macs-Mac-mini-2.local>
2024-01-06 14:46:15 +0100

    some ui

---
+++ app/@auth/_components/LoginSvg.tsx
@@ -0,0 +1,11 @@
+// LoginSvg.js
+import Image from 'next/image';
+import React from 'react';
+
+const LoginSvg = () => {
+  return (
+    <Image src="auth/loginSvg.svg" alt="LoginSvg" width={400} height={250} className=" max-xl:w-[75%]" />
+  );
+};
+
+export default LoginSvg;
