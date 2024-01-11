5b11693cf3faa3d6ca8bffb8cca6cec06dd1ce29
Souhail Brahmi <souhail.brahmi@horizon-tech.tn>
2024-01-11 17:46:09 +0100

    feat: adding ui for creating subjects

---
+++ app/@teacher/[etab_id]/(teacherdashboard)/(routes)/settings/subjects/_components/EstablishementsList.tsx
@@ -0,0 +1,301 @@
+'use client';
+
+import * as React from 'react';
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+import { ArrowUpDown, ChevronDown, MoreHorizontal } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+import { Checkbox } from '@/components/ui/checkbox';
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Input } from '@/components/ui/input';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import Image from 'next/image';
+import { ModifierUnEtudiant } from '@/components/modals/modifier-un-etudiant';
+import { CorrectExam } from '@/components/modals/correct-exam';
+
+const data: Payment[] = [
+  {
+    id: 'm5gr84i9',
+
+    name: 'école Privée les étoiles',
+  },
+  {
+    id: '3u1reuv4',
+    name: 'Lycée Privé élite Nabeul',
+  },
+  {
+    id: 'derv1ws0',
+    name: 'Lycée Al Manar',
+  },
+];
+
+export type Payment = {
+  id: string;
+  name: string;
+};
+
+export const CorrectionTag = ({
+  correction,
+  color,
+  width,
+  bgcolor,
+}: {
+  correction: 'En cours' | 'Non corrigé' | 'Corrigé' | 'Absent' | 'Non classé';
+  color: string;
+  width: string;
+  bgcolor: string;
+}) => (
+  <div
+    className={`w-[${width}] h-[22px] px-[5px]  rounded-2xl justify-center items-center gap-[5px] inline-flex`}
+    style={{ backgroundColor: `${bgcolor}` }}
+  >
+    <div className={`w-1.5 h-1.5 rounded-full`} style={{ backgroundColor: `${color}` }} />
+    <div className={` text-xs font-normal leading-[18px]`} style={{ color: `${color}` }}>
+      {correction}
+    </div>
+  </div>
+);
+export const columns: ColumnDef<Payment>[] = [
+  {
+    id: 'select',
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() || (table.getIsSomePageRowsSelected() && 'indeterminate')
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+        className="bg-white"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  // {
+  //   accessorKey: 'rang',
+  //   header: () => {
+  //     return <span className="text-[#1B8392] ">Rang</span>;
+  //   },
+  //   cell: ({ row }) => (
+  //     <div className="w-10 h-[21px] p-2.5 bg-[#D8ECF3] rounded-[10px] border border-[#1B8392] flex-col justify-center items-center gap-2.5 inline-flex">
+  //       <div className="text-center text-[#1B8392] text-sm font-semibold ">
+  //         {row.getValue('rang')}
+  //       </div>
+  //     </div>
+  //   ),
+  // },
+  {
+    accessorKey: 'name',
+    header: ({ column }) => {
+      return (
+        <Button
+          className="text-[#1B8392]  hover:text-[#1B8392]"
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Nom de l&apos;etablissement
+          <ArrowUpDown className="w-4 h-4 ml-2" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => (
+      <div className="flex items-center gap-2 capitalize">
+        {/* <Image src="/defaultUserAvatr.svg" alt="" width={42} height={42} className="rounded-full" /> */}
+        {row.getValue('name')}
+      </div>
+    ),
+  },
+
+
+  {
+    header: () => {
+      return <span className="text-[#1B8392] w-[205px] ">Actions</span>;
+    },
+    id: 'actions',
+    enableHiding: false,
+
+    cell: () => {
+      return (
+        <div className="flex items-center gap-4 " style={{ width: '50px' }}>
+          {/* <ModifierUnEtudiant>
+            <Image src="/eyesicon.svg" alt="" width={20} height={20} className="cursor-pointer " />
+          </ModifierUnEtudiant> */}
+
+          {/* <CorrectExam>
+            <Image
+              src="/correctionExam.svg"
+              alt=""
+              width={20}
+              height={20}
+              className="cursor-pointer "
+            />
+          </CorrectExam> */}
+          {/* <Image
+            src="/invitestudent.svg"
+            alt=""
+            width={20}
+            height={20}
+            className="cursor-pointer "
+          /> */}
+
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="w-8 h-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal className="w-4 h-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              {/* <DropdownMenuItem> */}
+              <ModifierUnEtudiant>
+                {/* <Image
+                  src="/eyesicon.svg"
+                  alt=""
+                  width={20}
+                  height={20}
+                  className="cursor-pointer "
+                /> */}
+                <p className="rounded-sm px-2 py-1.5 text-sm transition-colors hover:bg-accent ">
+                  Modifier
+                </p>
+                {/* <DropdownMenuItem>Modifier</DropdownMenuItem> */}
+              </ModifierUnEtudiant>
+
+              <DropdownMenuItem>Supprimer</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      );
+    },
+  },
+];
+
+export function EstablishementsList() {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([]);
+  const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = React.useState({});
+
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  });
+
+  return (
+    <div className="w-full">
+      <div className="border rounded-md ">
+        <Table>
+          <TableHeader className="bg-[#F0F6F8] text-[#1B8392]">
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(header.column.columnDef.header, header.getContext())}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-lg text-center bg-transparent"
+                >
+                  Pas d&apos;establishementsList ajoutés.
+                  <span className="text-[#1B8392]">Ajoutez</span> enseignants ou{' '}
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end py-4 space-x-2">
+        <div className="flex-1 text-sm text-muted-foreground">
+          {table.getFilteredSelectedRowModel().rows.length} of{' '}
+          {table.getFilteredRowModel().rows.length} row(s) selected.
+        </div>
+        <div className="space-x-2">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/@teacher/[etab_id]/(teacherdashboard)/(routes)/settings/subjects/page.tsx
+++ app/@teacher/[etab_id]/(teacherdashboard)/(routes)/settings/subjects/page.tsx
@@ -1,9 +1,98 @@
-function page() {
-    return (
-        <div>
-            subjects
-        </div>
-    )
-}
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import Image from 'next/image';
+import Link from 'next/link';
+import  {EstablishementsList}  from './_components/EstablishementsList';
+import { ImportUneClasse } from '@/components/modals/importer-une-classe';
+import { AjouterUnEtudiant } from '@/components/modals/ajouter-un-etudiant';
+import { AddEstab } from '@/components/modals/AddEstab';
 
-export default page
+const Establishement = ({ params }: { params: { classesId: string } }) => {
+  const { classesId } = params;
+  const handleImportedData = (jsonData: any) => {
+    // Handle the imported data in the external page
+    console.log(jsonData);
+  };
+
+  return (
+    <main className="flex flex-col gap-6 p-10">
+      <nav className="flex justify-between w-full ">
+        <div className="flex flex-col gap-4">
+          <div className="text-[#1B8392] text-2xl font-semibold ">Paramètres</div>
+          <div className="flex items-center text-[#727272]">
+            {/* <Image src="/arrowleft.svg" alt="icons" width={20} height={20} /> */}
+
+            {/* <Link href={'/settings'} className="cursor-pointer">
+              Classes
+            </Link> */}
+            <Image src="/arrowleft.svg" alt="icons" width={20} height={20} />
+
+            <span className="cursor-pointer">Établissements</span>
+          </div>
+        </div>
+
+        <div className="flex gap-3 pt-4 h-14 cursor-pointe ">
+          {/* <Select>
+            <SelectTrigger className="flex items-center p-2 border rounded-lg cursor-pointer text-[#1B8392]  border-[#99C6D3] gap-3 hover:opacity-80 w-[146px]">
+              <SelectValue
+                placeholder={
+                  <div className="flex items-center">
+                    <Image src={'/filterIcon.svg'} alt="filtericon" width={20} height={20} />
+                    <span className="ml-2 text-[#1B8392] text-base  ">Filter</span>
+                  </div>
+                }
+              />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="corrige" className="">
+                Corrigé
+              </SelectItem>
+              <SelectItem value="en-cours">En cours</SelectItem>
+              <SelectItem value="non-corrigé">Non corrigé</SelectItem>
+              <SelectItem value="non-classé">Non classé</SelectItem>
+              <SelectItem value="absent">Absent</SelectItem>
+            </SelectContent>
+          </Select> */}
+
+          {/* importer */}
+          {/* <ImportUneClasse>
+            <div className=" justify-center p-2 border rounded-lg cursor-pointer bg-[#1B8392] text-white gap-1 hover:opacity-80 flex items-center">
+              <Image src="/importerIcon.svg" alt="icons" width={20} height={20} />
+              <div className="pl-2 pr-2 text-sm font-semibold leading-tight text-center ">
+                Importer
+              </div>
+            </div>
+          </ImportUneClasse> */}
+
+          <div className="flex items-center p-2 border rounded-lg cursor-pointer border-[#99C6D3] gap-3 hover:opacity-80 ">
+            <Image src="/scoop.svg" alt="icons" width={20} height={20} />
+
+            <input
+              type="text"
+              placeholder="Recherche"
+              className=" w-24 bg-transparent outline-none border-none  text-sm font-semibold  leading-tight placeholder-[#99C6D3]"
+            />
+          </div>
+          <AddEstab>
+            <div className="flex items-center p-2 border rounded-lg cursor-pointer bg-[#1B8392] text-white gap-3 hover:opacity-80 ">
+              <div className="pl-2 pr-2 text-sm font-semibold leading-tight text-center ">
+                Ajouter une établissements
+              </div>
+            </div>
+          </AddEstab>
+        </div>
+      </nav>
+
+      <div>
+        <EstablishementsList />
+      </div>
+    </main>
+  );
+};
+
+export default Establishement;
