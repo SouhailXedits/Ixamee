02bc342a097cd60b2a6cf1b35fb0cbafaae06c29
Souhail Brahmi <souhail.brahmi@horizon-tech.tn>
2024-01-17 18:46:28 +0100

    fixing biulding errors

--- app/@teacher/[etab_id]/(teacherdashboard)/_components/etablissement-item.tsx
+++ app/@teacher/[etab_id]/(teacherdashboard)/_components/etablissement-item.tsx
@@ -98,7 +98,6 @@ export function EtablissementItem({ data }: EtablissementItemProps) {
                       >
                         <FormControl>
                           <RadioGroupItem
-                          
                             id={''+lyce?.id} // Add an id for each item
                             value={''+lyce?.id}
                             className="text-[#FBB800] border-[#F0F6F8] w-4 h-4"
