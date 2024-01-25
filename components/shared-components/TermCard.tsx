71ab78445fc963ec28239af7a05ddb3e98999488
Souhail Brahmi <souhail.brahmi@horizon-tech.tn>
2024-01-25 09:26:55 +0100

    style: fix style fot TermCard

--- components/shared-components/TermCard.tsx
+++ components/shared-components/TermCard.tsx
@@ -4,7 +4,7 @@ import UserExam from './UserExam';
 function TermCard({ term }: any) {
   return (
     <div className=" flex flex-col gap-5  min-w-fit min-h-[15rem] h-full ">
-      <div className=" bg-4 p-2 rounded text-mainGreen font-semibold">{term.name}</div>
+      <div className=" bg-4/50 p-2 rounded text-mainGreen font-semibold">{term.name}</div>
       <div className=" min-w-fit min-h-[15rem] h-full flex flex-col gap-6 shadow-lg rounded-xl p-4 hover:scale-105 transition-all">
         <div className=" flex flex-col gap-4">
           {term.exams.length === 0 && <NoResultsFount />}
