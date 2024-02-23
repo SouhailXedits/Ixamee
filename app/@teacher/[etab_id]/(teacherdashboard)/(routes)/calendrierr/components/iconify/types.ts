96137ce4b0d51e52c9efcd216b17ab24e819b949
Souhail Brahmi <souhail.brahmi@horizon-tech.tn>
2024-02-23 08:30:12 +0100

    fix : fix pagination in admin routes

--- app/@teacher/[etab_id]/(teacherdashboard)/(routes)/calendrierr/components/iconify/types.ts
+++
@@ -1,6 +0,0 @@
-//@ts-ignore
-import { IconifyIcon } from '@iconify/react';
-
-// ----------------------------------------------------------------------
-
-export type IconifyProps = IconifyIcon | string;
--- app/@teacher/[etab_id]/(teacherdashboard)/(routes)/settings/subjects/_components/SubjectsList.tsx
+++ app/@teacher/[etab_id]/(teacherdashboard)/(routes)/settings/subjects/_components/SubjectsList.tsx
@@ -215,8 +215,7 @@ export function SubjectsList({
     table.nextPage();
   }
   function handlePreviousPage() {
-    // const cur = table.getPageCount();
-    if (currentpage === 0) onPageChange(currentpage - 1);
+    onPageChange(currentpage - 1);
     table.previousPage();
   }
 
