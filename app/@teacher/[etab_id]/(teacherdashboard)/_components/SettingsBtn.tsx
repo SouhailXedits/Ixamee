ccdf799bbb0b98fdc37b606ec98e45c4712382c6
Souhail Brahmi <souhail.brahmi@horizon-tech.tn>
2024-02-23 15:01:47 +0100

    feat: settings sidebar routes is now outside sidebar

--- app/@teacher/[etab_id]/(teacherdashboard)/_components/SettingsBtn.tsx
+++ app/@teacher/[etab_id]/(teacherdashboard)/_components/SettingsBtn.tsx
@@ -7,27 +7,40 @@ import { boolean } from 'zod';
 interface settingBtnProps {
   onClick: () => void;
   isActive: boolean;
+  isParameters?: boolean;
 }
-function SettingsBtn({ onClick, isActive }: settingBtnProps) {
+function SettingsBtn({ onClick, isActive, isParameters }: settingBtnProps) {
   const { collapsed } = useSidebar((state) => state);
-
+  
   return (
     <p
       className={cn(
         'flex items-start w-full  text-slate-500 text-sm font-[400] pl-3 transition-all',
-        !isActive && '  hover:bg-slate-300/20  hover:rounded-lg duration-300',
-        isActive && ' w-[190px] text-mainGreen rounded-lg'
+        // !isActive && ' hover:bg-slate-300/20  hover:rounded-lg duration-300',
+        // isActive && !isParameters && ' w-[190px] text-mainGreen rounded-lg'
       )}
     >
       <span className={cn('flex items-center w-full text-lg w gap-x-3 p-1')}>
-        <Image
-          src={isActive ? '/settings-fill.svg' : '/settings.svg'}
-          // src={'/icons/kebab-menu.svg'}
-          alt="settings icon"
-          width={collapsed ? 20 : 20}
-          height={collapsed ? 20 : 20}
-          className={cn('relative ', isActive && 'text-white  ')}
-        />
+        {!isParameters && (
+          <Image
+            src={isActive ? '/settings-fill.svg' : '/settings.svg'}
+            // src={'/icons/kebab-menu.svg'}
+            alt="settings icon"
+            width={collapsed ? 20 : 20}
+            height={collapsed ? 20 : 20}
+            className={cn('relative ', isActive && 'text-white  ')}
+          />
+        )}
+        {isParameters && (
+          <Image
+            src={ '/settings.svg'}
+            // src={'/icons/kebab-menu.svg'}
+            alt="settings icon"
+            width={collapsed ? 20 : 20}
+            height={collapsed ? 20 : 20}
+            className={cn('relative ', isActive && 'text-white  ')}
+          />
+        )}
 
         {collapsed ? '' : <span className=" font-[400]">Paramètres</span>}
       </span>
--- components/shared-components/sidebar-item.tsx
+++ components/shared-components/sidebar-item.tsx
@@ -10,12 +10,22 @@ interface SidebarItemProps {
   Defaulticon: string;
   label: string;
   href?: string;
+  isParameters?: boolean;
+  onClick?: () => void;
 }
-export const SidebarItem = ({ Clickedicon, Defaulticon, label, href }: SidebarItemProps) => {
+export const SidebarItem = ({
+  Clickedicon,
+  Defaulticon,
+  label,
+  href,
+  isParameters,
+  onClick: onClickHandler,
+}: SidebarItemProps) => {
   const params = useParams();
   const pathname = usePathname();
   const router = useRouter();
   const { collapsed } = useSidebar((state) => state);
+
   const isActive =
     (pathname === `/${params.etab_id}` && href === `/`) ||
     pathname === href ||
@@ -24,6 +34,7 @@ export const SidebarItem = ({ Clickedicon, Defaulticon, label, href }: SidebarIt
 
   const onClick = () => {
     if (href) router.push(`/${params.etab_id}${href}`);
+    if (onClickHandler) onClickHandler()
   };
 
   return (
@@ -33,20 +44,35 @@ export const SidebarItem = ({ Clickedicon, Defaulticon, label, href }: SidebarIt
       className={cn(
         'flex items-start w-full  text-slate-500 text-sm font-[400] pl-3 transition-all pt-[4px] pb-[4px] ',
         !isActive && 'hover:bg-slate-300/20 hover:bg-[#f0f6f82a] hover:rounded-lg duration-300',
-        isActive && 'bg-white   w-full text-mainGreen rounded-lg'
+        isActive && 'bg-white   w-full text-mainGreen rounded-lg',
+        isParameters && 'bg-2 rounded-lg text-white hover:bg-2/70',
+        isParameters && isActive && 'bg-2/70'
       )}
     >
       <div className={cn('flex items-center w-full text-lg w gap-x-3 p-1', isActive && 'p-1')}>
-        <Image
-          src={isActive ? Clickedicon : Defaulticon}
-          // src={'/icons/kebab-menu.svg'}
-          alt={Clickedicon}
-          width={collapsed ? 22 : 22}
-          height={collapsed ? 22 : 22}
-          className={cn('relative ', isActive && 'text-white  ')}
-        />
+        {isParameters && (
+          <Image
+            src={Defaulticon}
+            // src={'/icons/kebab-menu.svg'}
+            alt={Clickedicon}
+            width={collapsed ? 22 : 22}
+            height={collapsed ? 22 : 22}
+            // className={cn('relative ', isActive && 'text-white  ')}
+          />
+        )}
+        {!isParameters && (
+          <Image
+            src={isActive ? Clickedicon : Defaulticon}
+            // src={'/icons/kebab-menu.svg'}
+            alt={Clickedicon}
+            width={collapsed ? 22 : 22}
+            height={collapsed ? 22 : 22}
+            className={cn('relative ', isActive && 'text-white  ')}
+          />
+        )}
 
         {collapsed ? '' : <span className=" font-[400]">{label}</span>}
+        {isParameters && <span className=" font-[400]">{label}</span>}
       </div>
     </button>
   );
