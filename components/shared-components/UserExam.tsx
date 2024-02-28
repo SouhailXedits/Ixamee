3fe49f760939766f272c880e61da104af1283e2d
Souhail Brahmi <souhail.brahmi@horizon-tech.tn>
2024-02-28 12:27:06 +0100

    fix: fix th resuls crashing in the student

--- components/shared-components/UserExam.tsx
+++ components/shared-components/UserExam.tsx
@@ -7,8 +7,8 @@ import TelachargePdfEvaluation from './TelachargePdfEvaluation';
 function UserExam({ exam, examsData }: any) { 
 
 
-  const userContent = exam?.examCorrection[0];
-  const user_id = exam?.user.id;
+  const userContent = exam?.examCorrection?.[0];
+  const user_id = exam?.user?.id;
 
   const examContent = exam?.examContent;
 
@@ -32,13 +32,13 @@ function UserExam({ exam, examsData }: any) {
   return (
     <div className="flex items-center justify-between p-2 border-l-2 rounded border-orangeColor/80 gap-14">
       <div className=" basis-[50%]">
-        <p className="text-xl font-medium whitespace-nowrap text-black/80">{exam.name}</p>
-        <p className=" text-black/50">Ajouté le: {exam.date}</p>
+        <p className="text-xl font-medium whitespace-nowrap text-black/80">{exam?.name}</p>
+        <p className=" text-black/50">Ajouté le: {exam?.date}</p>
       </div>
       <div className=" flex items-center flex-col gap-1 text-mainGreen">
-        {exam.isPublished && (
+        {exam?.isPublished && (
           <p>
-            {exam.marksObtained}/{exam.totalScore}
+            {exam?.marksObtained}/{exam?.totalScore}
           </p>
         )}
 
@@ -46,7 +46,7 @@ function UserExam({ exam, examsData }: any) {
       </div>
       <div className="flex flex-col items-center gap-1 opacity-50 text-mainGreen">
         <p>rang </p>
-        {exam.isPublished ? <p>{exam.range}</p> : <p>--</p>}
+        {exam?.isPublished ? <p>{exam?.range}</p> : <p>--</p>}
       </div>
       <div className="flex flex-col items-center justify-center gap-0">
         <Button
