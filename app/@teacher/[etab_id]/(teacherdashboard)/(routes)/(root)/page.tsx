2111a5c9f71e56ea3a70f6eef5a83c23b04eae31
Souhail Brahmi <souhail.brahmi@horizon-tech.tn>
2024-03-05 09:35:29 +0100

    style: adding animation to the dashboard

--- app/@teacher/[etab_id]/(teacherdashboard)/(routes)/(root)/page.tsx
+++ app/@teacher/[etab_id]/(teacherdashboard)/(routes)/(root)/page.tsx
@@ -8,11 +8,17 @@ import { getCountOfClasse, getCountOfExamenes, getCountMonArchive } from '@/acti
 import { getAllClasseByPage, getAllClassesNameAndId, getStudentClassCount } from '@/actions/classe';
 import Loading from '@/app/loading';
 import DashboradStatistiques from './_components/dashborad-statistiques';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
   const queryClient = useQueryClient();
   const user = queryClient.getQueryData(['user']) as any;
   const etab_id = queryClient.getQueryData(['etab_id']) as any;
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    setAnimate(true);
+  }, []);
 
   const { data: classeCount, isPending: classeCountPending } = useQuery({
     queryKey: ['dashClasseCount'],
@@ -43,40 +49,46 @@ export default function Home() {
     queryFn: async () => await getAllClassesNameAndId({ user_id: user?.id, etab_id }),
   });
 
-
   return isPendingStudentClasse ? (
     <Loading />
   ) : (
-    <div className="flex flex-col w-full overflow-auto p-9">
+    <div className={`flex flex-col w-full overflow-auto p-9 `}>
       <div className="text-2 text-2xl font-[500] pl-4 ">Tableau de bord</div>
       <div className="flex gap-6 pt-10 flex-nowrap max-2xl:flex-wrap">
-        {/* first section 👺  */}
-        <div className=" flex flex-col gap-6 w-[60%] max-2xl:w-[100%] ">
-          <DashboradApercu
-            classeCount={classeCount}
-            examCount={examCount}
-            archiveCount={archiveCount}
-            classeCountPending={classeCountPending}
-            examCountPending={examCountPending}
-            archiveCountPending={archiveCountPending}
-            studentCount={studentCount?.data}
-            studentCountPending={isPendingStudentClasse}
-          />
-          <DashboradStatistiques
-            isPendingClasses={isPendingClasses}
-            classes={classes?.data}
-            allStudentCount={studentCount?.data}
-            studentCountPending={isPendingStudentClasse}
-          />
-          <DashboradClasses
-            classe={classe?.data}
-            classeCount={classeCount}
-            isPending={isPendingClasse}
-            etabId={etab_id}
-          />
+        <div className={`flex flex-col gap-6 w-[60%] max-2xl:w-[100%] `}>
+          <div className={`${animate ? 'animate' : ''}`}>
+            <DashboradApercu
+              classeCount={classeCount}
+              examCount={examCount}
+              archiveCount={archiveCount}
+              classeCountPending={classeCountPending}
+              examCountPending={examCountPending}
+              archiveCountPending={archiveCountPending}
+              studentCount={studentCount?.data}
+              studentCountPending={isPendingStudentClasse}
+            />
+          </div>
+          <div className={`flex flex-col gap-6 w-full ${animate ? 'animate-btt' : ''}`}>
+            <DashboradStatistiques
+              isPendingClasses={isPendingClasses}
+              classes={classes?.data}
+              allStudentCount={studentCount?.data}
+              studentCountPending={isPendingStudentClasse}
+            />
+            <DashboradClasses
+              classe={classe?.data}
+              classeCount={classeCount}
+              isPending={isPendingClasse}
+              etabId={etab_id}
+            />
+          </div>
         </div>
 
-        <div className="w-[40%] h-full p-2 flex flex-col gap-6 max-2xl:w-[100%]">
+        <div
+          className={`w-[40%] h-full p-2 flex flex-col gap-6 max-2xl:w-[100%] ${
+            animate ? 'animate-rtl' : ''
+          } `}
+        >
           <DashboradCorrectionsRecentes etabId={etab_id} classes={classes?.data} />
           <DashboradBulletinsDesEtudiants etabId={etab_id} classes={classes?.data} />
         </div>
