9732dc82ecf9da79f361a682f945573c6cea8164
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:33:08 +0100

    improve

--- next.config.js
+++ next.config.js
@@ -8,12 +8,21 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
-
-
-
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
 module.exports = withBundleAnalyzer(nextConfig);
+
+// The above code can be simplified to the following, if you are not using any other plugins with Next.js:
+
+// module.exports = withBundleAnalyzer({
+//   output: 'standalone',
+//   swcMinify: true,
+//   images: {
+//     domains: ['lh3.googleusercontent.com', 'res.cloudinary.com'],
+//   },
+//   analyzer: {
+//     enabled: process.env.ANALYZE === 'true',
+//   },
+// });
