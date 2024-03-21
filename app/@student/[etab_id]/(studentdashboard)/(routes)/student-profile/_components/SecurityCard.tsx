c9b6b15b1f43e5f89da5873c964e350b20fdaf31
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:32:55 +0100

    improve

--- app/@student/[etab_id]/(studentdashboard)/(routes)/student-profile/_components/SecurityCard.tsx
+++ app/@student/[etab_id]/(studentdashboard)/(routes)/student-profile/_components/SecurityCard.tsx
@@ -1,29 +1,33 @@
 import React from 'react';
-import AuthSvg from './AuthSvg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
 import { UpdatePassword } from './UpdatePassword';
 
-export default function SecurityCard({ user }: any) {
+type SecurityCardProps = {
+  user: {
+    password?: string;
+  };
+};
+
+type UpdatePasswordProps = {
+  currentUser: {
+    password?: string;
+  };
+  title: string;
+  disabled: boolean;
+};
+
+export default function SecurityCard({ user }: SecurityCardProps) {
+  const { password } = user;
+  const isGoogleUser = !password;
+
   return (
-    <div className="w-[50%] h-full  p-5 flex flex-col gap-6  rounded-xl shadow-lg max-xl:w-full">
+    <div className="w-[50%] h-full p-5 flex flex-col gap-6 rounded-xl shadow-lg max-xl:w-full">
       <div className="text-[#1B8392] text-lg">Sécurité et authentification</div>
       <div className="flex items-center justify-center">
-        <AuthSvg className={' max-xl:w-[50%]'} width={300} height={200} />
+        <FontAwesomeIcon icon={faLock} className="text-gray-500 w-16 h-16" />
       </div>
-      {user.password ? (
-        <div className="flex justify-center w-full cursor-pointer ">
-          <UpdatePassword currentUser={user}>
-            <div className="text-red border-solid border-2  border-red w-full flex items-center justify-center p-3 rounded-lg ">
-              Modifier mot de passe
-            </div>
-          </UpdatePassword>
-        </div>
-      ) : (
-        <div className="flex justify-center w-full cursor-pointer ">
-          <div className="text-red border-solid border-2  border-red w-full flex items-center justify-center p-3 rounded-lg ">
-            Vous êtes de Google, vous ne pouvez pas mettre à jour votre mot de passe ixamee
-          </div>
-        </div>
-      )}
-    </div>
-  );
-}
+      <UpdatePassword
+        currentUser={user}
+        title={isGoogleUser ? 'Mot de passe Google' : 'Modifier mot de passe'}
+        disabled={
