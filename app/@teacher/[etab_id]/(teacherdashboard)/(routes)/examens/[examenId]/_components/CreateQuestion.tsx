fa6fa084d065c5577a8952671f309277aef56c72
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:33:09 +0100

    improve

--- app/@teacher/[etab_id]/(teacherdashboard)/(routes)/examens/[examenId]/_components/CreateQuestion.tsx
+++ app/@teacher/[etab_id]/(teacherdashboard)/(routes)/examens/[examenId]/_components/CreateQuestion.tsx
@@ -5,158 +5,117 @@ import Editor from '../../../../../../../../components/shared-components/toolbar
 import { cn } from '@/lib/utils';
 import toast from 'react-hot-toast';
 
-export const CreateQuestion = ({ allData, data, setFakeData, isArabic, fakeData }: any) => {
-  //  this the content of the Editor 🙄
+interface QuestionProps {
+  allData: any;
+  data: any;
+  setFakeData: (data: any) => void;
+  isArabic: boolean;
+  fakeData: any;
+}
+
+export const CreateQuestion = ({ allData, data, setFakeData, isArabic, fakeData }: QuestionProps) => {
   const onChange = (content: string) => {
     updateContetn(content, data);
   };
-  // Function to create and add a new subquestion to a specific question in fake data
+
   const createSubQuestion = () => {
-    // Generating a new subquestion with a random id, name, initial mark, and an empty children array
     const newSubQuestion = {
       id: Math.random().toString(36).substring(7),
-      name: `${data.children.length + 1})`, // 🆕 Naming the subquestion based on the current number of children
+      name: `${data.children.length + 1})`,
       mark: 1,
       children: [],
     };
 
-    // Update the state to include the new subquestion
     setFakeData((prevData: any) => {
-      // Mapping over the previous data to create a new updatedData array
       return prevData.map((item: any) => {
-        // Checking if the current item's id matches the id of the parent question
         if (item.id === allData.id) {
-          // Updating the children array of the parent question
           return {
             ...item,
             children: item.children.map((subItem: any) => {
-              // Checking if the current subitem's id matches the id of the parent subquestion
               if (subItem.id === data.id) {
-                // Adding the new subquestion to the children array of the parent subquestion
                 return {
                   ...subItem,
                   children: [...subItem.children, newSubQuestion],
                 };
               }
-              // Returning unchanged subitems
               return subItem;
             }),
           };
         }
-        // Returning unchanged items
         return item;
       });
     });
   };
 
-  // Function to handle deleting a question from fake data
   const handelDeleteQuestion = () => {
-    // 🚨 Logging the previous data for debugging purposes
     setFakeData((prevData: any) => {
-      // Mapping over the previous data to create a new updatedData array
       return prevData.map((item: any) => {
-        // Checking if the current item's id matches the id of the question to be deleted
         if (item.id === allData.id) {
-          // 🗑️ Removing the specific child with the matching id from the children array
           return {
             ...item,
             children: item.children.filter((subItem: any) => subItem.id !== data.id),
           };
         }
-        // Returning unchanged items
         return item;
       });
     });
-    // 🔄 Triggering a re-render of questions after deletion
     handeRenderQuestion();
   };
 
-  // Function to handle rendering questions and updating their names
   const handeRenderQuestion = () => {
-    // 🚨 Logging the previous data for debugging purposes
     setFakeData((prevData: any) => {
-      // Mapping over the previous data to create a new updatedData array
       return prevData.map((item: any) => {
-        // Function to get the next character in a sequence based on the index
         const getNextChar = (num: number) => String.fromCharCode(0x2160 + num);
 
-        // Mapping over the children array of each item
         item.children.map((state: any, index: number) => {
-          // 🌟 Updating the name of each child with the next character in the sequence
           state.name = getNextChar(index);
         });
 
-        // Returning the updated item
         return item;
       });
     });
   };
 
-  /**
-   * Calculate the mark for the given data
-   * @param data - The data containing children and mark
-   * @returns The calculated mark
-   */
   const calculateMark = (data: any) => {
-    // If data has no children, default to an empty array
     const children = data?.children || [];
 
-    // Calculate the mark by summing up the marks of the children
     data.mark = children?.reduce((acc: number, item: any) => {
       return acc + item.mark;
     }, 0);
 
-    // Format the mark to have two decimal places
-    // const formattedMark = data?.mark?.toFixed(2);
-
-    // Return the formatted mark
     return data.mark;
   };
 
   const updateContetn = (contetn: string, data: any) => {
-    // Using setFakeData to update the state based on previous data
     setFakeData((prevData: any) => {
-      // Mapping over the previous data to create a new updatedData array
-      const updatedData = prevData.map((item: any) => {
-        // Mapping over the children array of each item
+      return prevData.map((item: any) => {
         item.children.map((state: any, index: number) => {
-          // Checking if the current child's id matches the provided data's id
           if (state.id === data.id) {
-            // Updating the mark of the matching child with the new value (+e)
             state.content = contetn;
           }
         });
-        // Returning the updated item
         return item;
       });
-      // Returning the updatedData to setFakeData
-      return updatedData;
     });
   };
 
-  // Function to update a question's mark in the fake data
   const updateQuestion = (e: any, data: any) => {
-    if (+e < 1) {
-      toast.error('la note ne doit pas etre inferieur a 0');
+    const value = parseFloat(e.target.value);
+
+    if (isNaN(value) || value < 0) {
+      toast.error('La note ne doit pas être inférieure à 0');
       return;
     }
-    // Using setFakeData to update the state based on previous data
+
     setFakeData((prevData: any) => {
-      // Mapping over the previous data to create a new updatedData array
-      const updatedData = prevData.map((item: any) => {
-        // Mapping over the children array of each item
+      return prevData.map((item: any) => {
         item.children.map((state: any, index: number) => {
-          // Checking if the current child's id matches the provided data's id
           if (state.id === data.id) {
-            // Updating the mark of the matching child with the new value (+e)
-            state.mark = +e;
+            state.mark = value;
           }
         });
-        // Returning the updated item
         return item;
       });
-      // Returning the updatedData to setFakeData
-      return updatedData;
     });
   };
 
@@ -182,7 +141,6 @@ export const CreateQuestion = ({ allData, data, setFakeData, isArabic, fakeData 
               <span>)</span>
             </div>
             <Editor
-              // initialContent={data.content}
               editable={true}
               onChange={onChange}
               initialContent={data.content}
@@ -195,12 +153,10 @@ export const CreateQuestion = ({ allData, data, setFakeData, isArabic, fakeData 
               type="number"
               min={1}
               step="0.25"
-              // value={data.mark}
-              disabled={data.children && data.children.length > 0}
-              // defaultValue={calculerQuestionMark(data)}
               value={data.children && data.children.length > 0 ? calculateMark(data) : data.mark}
+              disabled={data.children && data.children.length > 0}
               onChange={(e) => {
-                updateQuestion(e.target.value, data);
+                updateQuestion(e, data);
               }}
             />
             <Image
@@ -212,18 +168,4 @@ export const CreateQuestion = ({ allData, data, setFakeData, isArabic, fakeData 
               onClick={() => handelDeleteQuestion()}
             />
           </div>
-        </div>
-      </div>
-      {data.children.map((item: any) => (
-        <CreateSubQuestion
-          key={item.id}
-          allData={allData}
-          data={item}
-          setFakeData={setFakeData}
-          isArabic={isArabic}
-          fakeData={fakeData}
-        />
-      ))}
-    </>
-  );
-};
+       
