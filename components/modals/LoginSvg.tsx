1bee494fa8be7353e69e11d09d4235a198701808
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:33:25 +0100

    improve

--- components/modals/LoginSvg.tsx
+++ components/modals/LoginSvg.tsx
@@ -1,13 +1,21 @@
 // LoginSvg.js
 import Image from 'next/image';
-import React from 'react';
+import React, { forwardRef } from 'react';
 
-const LoginSvg = () => {
+const LoginSvg = forwardRef((props, ref) => {
   return (
     <div className="animate-bounce repeat-infinite max-xl:w-[75%] transition-all duration-1">
-      <Image src="/auth/loginSvg.svg" alt="LoginSvg" width={400} height={250} />
+      <Image
+        src="/auth/loginSvg.svg"
+        alt="LoginSvg"
+        width={400}
+        height={250}
+        decoding="async"
+        ref={ref}
+        {...props}
+      />
     </div>
   );
-};
+});
 
 export default LoginSvg;
