455a6898b4e5c0b4390c88f2d1ee9d1dd786b401
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:32:53 +0100

    improve

--- app/@teacher/[etab_id]/(teacherdashboard)/(routes)/(root)/_components/dashborad-bulletins-des-etudiants.tsx
+++ app/@teacher/[etab_id]/(teacherdashboard)/(routes)/(root)/_components/dashborad-bulletins-des-etudiants.tsx
@@ -3,22 +3,25 @@ import CorrectionsRecentes from './corrections-recentes-item';
 import BulletinsDesEtudiants from './bulletins-des-etudiants';
 import Link from 'next/link';
 
-const DashboradBulletinsDesEtudiants = ({ etabId, classes }: any) => {
+type DashboradBulletinsDesEtudiantsProps = {
+  etabId: string;
+  classes: any;
+};
 
-  
+const DashboradBulletinsDesEtudiants = ({ etabId, classes }: DashboradBulletinsDesEtudiantsProps) => {
   return (
     <div className="flex flex-col gap-4">
       <div className="flex justify-between w-full">
         <div className="flex items-center gap-4">
-          <Image src="dashboard/bulttin/bulletinsIcon.svg" alt="correct" width={21} height={21} />
-          <span className=" text-[#727272] text-xl font-[600] ">Bulletins des étudiants</span>
+          <Image src="/dashboard/bulttin/bulletinsIcon.svg" alt="bulletins" width={21} height={21} />
+          <span className="text-[#727272] text-xl font-[600]">Bulletins des étudiants</span>
         </div>
-        <div className="text-[#1B8392] cursor-pointer text-lg font-medium  underline">
+        <div className="text-[#1B8392] cursor-pointer text-lg font-medium underline">
           <Link href={`/${etabId}/bulletins`}>Voir plus (0)</Link>
         </div>
       </div>
 
-      <BulletinsDesEtudiants etabId={etabId} classes={classes}/>
+      <BulletinsDesEtudiants etabId={etabId} classes={classes} />
     </div>
   );
 };
