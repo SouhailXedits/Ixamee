c750e24b7eda4fb9e9c93c55bd37da0800d908f8
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:33:00 +0100

    improve

--- app/@teacher/[etab_id]/(teacherdashboard)/(routes)/examens/hooks/useEditeExam.ts
+++ app/@teacher/[etab_id]/(teacherdashboard)/(routes)/examens/hooks/useEditeExam.ts
@@ -1,24 +1,26 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-
+import { UseMutationResult, UseQueryClient } from '@tanstack/react-query';
 import { toast } from 'react-hot-toast';
 import { updateExam as updateExamApi } from '@/actions/examens';
 
-export function useEditExam() {
-  const queryClient = useQueryClient();
+export interface EditExamProps {
+  examId: number;
+  data: any;
+  userId: string;
+}
 
-  const { mutate: editExam, isPending } = useMutation({
-    mutationFn: ({ exam_id, data, user_id }: { exam_id: number; data: any; user_id: string }) =>
-      updateExamApi(exam_id, data, user_id), // Call your API function to update the exam
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['dashExamCount'] });
-      queryClient.invalidateQueries({ queryKey: ['examens'] });
-      toast.success('Exame édité avec succès.');
-    },
-    onError: (err) => {
-      toast.error("Une erreur est survenue lors de l'édition de l'exame.");
-    },
-    retry: false,
-  });
-
-  return { editExam, isPending };
+export function useEditExam(queryClient: UseQueryClient): UseMutationResult<void, Error, EditExamProps> {
+  return useMutation(
+    ({ examId, data, userId }: EditExamProps) => updateExamApi(examId, data, userId),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(['dashExamCount']);
+        queryClient.invalidateQueries(['examens']);
+        toast.success('Exame édité avec succès.');
+      },
+      onError: (error) => {
+        toast.error('Une erreur est survenue lors de l'édition de l\'exame: ' + error.message);
+      },
+      retry: false,
+    }
+  );
 }
