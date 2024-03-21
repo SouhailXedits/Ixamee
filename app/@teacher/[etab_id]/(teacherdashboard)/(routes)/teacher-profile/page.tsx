b7aee3d79f51ebea6d5a3e6a992bf1cab1d94c26
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:33:03 +0100

    improve

--- app/@teacher/[etab_id]/(teacherdashboard)/(routes)/teacher-profile/page.tsx
+++ app/@teacher/[etab_id]/(teacherdashboard)/(routes)/teacher-profile/page.tsx
@@ -9,17 +9,23 @@ const Profile = () => {
 
   const user = queryClient.getQueryData(['user']) as any;
 
-  const { data: userEstablishment, isPending: isPendingEstablishment } = useQuery<any>({
+  const { data: userEstablishment, isPending: isPendingEstablishment, error: errorEstablishment, ...userEstablishmentQuery } = useQuery<any>({
     queryKey: ['AllEstabOfUser'],
     queryFn: async () => await getEstablishmentOfUser(user?.id as string),
     staleTime: 0,
+    onError: (error: any) => console.error('Error fetching user establishments: ', error),
   });
 
-  const { data: teacherSubject, isPending: isPendingSubject } = useQuery<any>({
+  const { data: teacherSubject, isPending: isPendingSubject, error: errorSubject, ...teacherSubjectQuery } = useQuery<any>({
     queryKey: ['teacherSubjects'],
     queryFn: async () => await getSubjectOfUserById(user?.id as string),
     staleTime: 0,
+    onError: (error: any) => console.error('Error fetching teacher subjects: ', error),
   });
+
+  if (!user) {
+    return <div>No user found</div>;
+  }
 
   return (
     <main className="flex flex-col gap-6 p-10">
@@ -27,27 +33,14 @@ const Profile = () => {
         <div className="flex flex-col gap-4">
           <div className="text-[#1B8392] text-2xl font-semibold ">Mon profil</div>
           <div className="flex items-center text-[#727272]">
-            <Image src="/arrowleft.svg" alt="icons" width={20} height={20} />
+            <Image src="/arrowleft.svg" alt="Back arrow" width={20} height={20} />
             <span className="cursor-pointer">Mon profil</span>
           </div>
         </div>
       </nav>
 
       <div>
-        {isPendingSubject ? (
+        {isPendingSubject || isPendingEstablishment ? (
           <div className="flex items-center justify-center">
             <div className="animate-spin border-mainGreen rounded-full h-8 w-8 border-t-2 border-blue-500 border-r-2 border-b-2 border-gray-300"></div>
-          </div>
-        ) : (
-          <ProfileCards
-            user={user}
-            userEstablishment={userEstablishment}
-            teachersubject={teacherSubject}
-          />
-        )}
-      </div>
-    </main>
-  );
-};
-
-export default Profile;
+          
