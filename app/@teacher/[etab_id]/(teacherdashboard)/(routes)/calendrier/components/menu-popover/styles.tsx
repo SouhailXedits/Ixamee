25446057961f8cbbf3d6f4fd1c3e1f6c86d4f32a
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:33:01 +0100

    improve

--- app/@teacher/[etab_id]/(teacherdashboard)/(routes)/calendrier/components/menu-popover/styles.tsx
+++ app/@teacher/[etab_id]/(teacherdashboard)/(routes)/calendrier/components/menu-popover/styles.tsx
@@ -1,72 +1,97 @@
 // @mui
 import { styled, alpha } from '@mui/material/styles';
-//
 import { MenuPopoverArrowValue } from './types';
 
-// ----------------------------------------------------------------------
-
-export const StyledArrow = styled('span')<{ arrow: MenuPopoverArrowValue }>(({ arrow, theme }) => {
+//
+const getArrowStyle = (arrow: MenuPopoverArrowValue, theme: any) => {
   const SIZE = 12;
-
   const POSITION = -(SIZE / 2);
-
   const borderStyle = `solid 1px ${alpha(theme.palette.grey[500], 0.12)}`;
 
-  const topStyle = {
-    borderRadius: '0 0 3px 0',
-    top: POSITION,
-    borderBottom: borderStyle,
-    borderRight: borderStyle,
-  };
-
-  const bottomStyle = {
-    borderRadius: '3px 0 0 0',
-    bottom: POSITION,
-    borderTop: borderStyle,
-    borderLeft: borderStyle,
-  };
-
-  const leftStyle = {
-    borderRadius: '0 3px 0 0',
-    left: POSITION,
-    borderTop: borderStyle,
-    borderRight: borderStyle,
-  };
-
-  const rightStyle = {
-    borderRadius: '0 0 0 3px',
-    right: POSITION,
-    borderBottom: borderStyle,
-    borderLeft: borderStyle,
-  };
-
-  return {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      zIndex: 1,
-      width: SIZE,
-      height: SIZE,
-      content: "''",
-      display: 'block',
-      position: 'absolute',
-      transform: 'rotate(-135deg)',
-      background: theme.palette.background.paper,
+  const arrowStyles = {
+    topLeft: {
+      borderRadius: '0 0 3px 0',
+      top: POSITION,
+      borderBottom: borderStyle,
+      borderRight: borderStyle,
     },
-    // Top
-    ...(arrow === 'top-left' && { ...topStyle, left: 20 }),
-    ...(arrow === 'top-center' && { ...topStyle, left: 0, right: 0, margin: 'auto' }),
-    ...(arrow === 'top-right' && { ...topStyle, right: 20 }),
-    // Bottom
-    ...(arrow === 'bottom-left' && { ...bottomStyle, left: 20 }),
-    ...(arrow === 'bottom-center' && { ...bottomStyle, left: 0, right: 0, margin: 'auto' }),
-    ...(arrow === 'bottom-right' && { ...bottomStyle, right: 20 }),
-    // Left
-    ...(arrow === 'left-top' && { ...leftStyle, top: 20 }),
-    ...(arrow === 'left-center' && { ...leftStyle, top: 0, bottom: 0, margin: 'auto' }),
-    ...(arrow === 'left-bottom' && { ...leftStyle, bottom: 20 }),
-    // Right
-    ...(arrow === 'right-top' && { ...rightStyle, top: 20 }),
-    ...(arrow === 'right-center' && { ...rightStyle, top: 0, bottom: 0, margin: 'auto' }),
-    ...(arrow === 'right-bottom' && { ...rightStyle, bottom: 20 }),
-  };
-});
+    topCenter: {
+      borderRadius: '0 0 3px 0',
+      top: POSITION,
+      borderBottom: borderStyle,
+      borderRight: borderStyle,
+      left: 0,
+      right: 0,
+      margin: 'auto',
+    },
+    topRight: {
+      borderRadius: '0 0 3px 0',
+      top: POSITION,
+      borderBottom: borderStyle,
+      borderRight: borderStyle,
+      right: 20,
+    },
+    bottomLeft: {
+      borderRadius: '3px 0 0 0',
+      bottom: POSITION,
+      borderTop: borderStyle,
+      borderLeft: borderStyle,
+      left: 20,
+    },
+    bottomCenter: {
+      borderRadius: '3px 0 0 0',
+      bottom: POSITION,
+      borderTop: borderStyle,
+      borderLeft: borderStyle,
+      left: 0,
+      right: 0,
+      margin: 'auto',
+    },
+    bottomRight: {
+      borderRadius: '3px 0 0 0',
+      bottom: POSITION,
+      borderTop: borderStyle,
+      borderLeft: borderStyle,
+      right: 20,
+    },
+    leftTop: {
+      borderRadius: '0 3px 0 0',
+      top: 20,
+      borderTop: borderStyle,
+      borderRight: borderStyle,
+      left: POSITION,
+    },
+    leftCenter: {
+      borderRadius: '0 3px 0 0',
+      top: 0,
+      bottom: 0,
+      margin: 'auto',
+      borderTop: borderStyle,
+      borderRight: borderStyle,
+      left: POSITION,
+    },
+    leftBottom: {
+      borderRadius: '0 3px 0 0',
+      bottom: 20,
+      borderTop: borderStyle,
+      borderRight: borderStyle,
+      left: POSITION,
+    },
+    rightTop: {
+      borderRadius: '0 0 0 3px',
+      top: 20,
+      borderBottom: borderStyle,
+      borderLeft: borderStyle,
+      right: POSITION,
+    },
+    rightCenter: {
+      borderRadius: '0 0 0 3px',
+      top: 0,
+      bottom: 0,
+      margin: 'auto',
+      borderBottom: borderStyle,
+      borderLeft: borderStyle,
+      right: POSITION,
+    },
+    rightBottom: {
+      borderRadius: '0 0 0 3px
