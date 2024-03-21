7762a361edaef152778fc7a07fb63043d92a490e
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:32:52 +0100

    improve

--- actions/auth/sendPasswordResetToken.ts
+++ actions/auth/sendPasswordResetToken.ts
@@ -1,9 +1,14 @@
-'use server';
+// sendPasswordResetToken.ts
 
 import { generatePasswordResetToken } from '@/lib/tokens';
 import { redirect } from 'next/navigation';
 
-export const sendPasswordResetToken = async (email: string) => {
-  const passwordResetToken = await generatePasswordResetToken(email);
-  redirect(`/new-password?token=${passwordResetToken.token}`);
-};
+export async function sendPasswordResetToken(email: string) {
+  try {
+    const passwordResetToken = await generatePasswordResetToken(email);
+    redirect(`/new-password?token=${passwordResetToken.token}`);
+  } catch (error) {
+    console.error('Error sending password reset token:', error);
+    throw new Error('An error occurred while sending the password reset token. Please try again later.');
+  }
+}
