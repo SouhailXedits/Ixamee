1d5b7f9943f190b6ccd49a8f5f18d61bcd0bb562
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:33:06 +0100

    improve

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,9 +1,10 @@
-import { Role } from '@prisma/client';
+import { Role, Establishment } from '@prisma/client';
 import NextAuth, { DefaultSession } from 'next-auth';
 
 export type ExtendedUser = DefaultSession['user'] & {
   role: Role;
-  establishement_id: number;
+  establishmentId: number;
+  establishment?: Establishment;
 };
 
 declare module 'next-auth' {
