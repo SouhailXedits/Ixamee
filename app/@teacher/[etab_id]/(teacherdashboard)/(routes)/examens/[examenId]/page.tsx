b2aac184b33004b69729a3d6ff7febee7888c016
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:33:18 +0100

    improve

--- app/@teacher/[etab_id]/(teacherdashboard)/(routes)/examens/[examenId]/page.tsx
+++ app/@teacher/[etab_id]/(teacherdashboard)/(routes)/examens/[examenId]/page.tsx
@@ -1,4 +1,3 @@
-'use client';
 import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
 import CreateExam from './_components/create-exam';
@@ -13,20 +12,28 @@ import { cn } from '@/lib/utils';
 import { calcAllMark } from '@/app/_utils/calculateChildrenMarks';
 import { useConfettiStore } from '@/store/use-confetti-store';
 
-export default function Page({ params }: { params: { examenId: string; etab_id: string } }) {
+interface PageProps {
+  params: {
+    examenId: string;
+    etab_id: string;
+  };
+}
+
+const ExamPage = ({ params }: PageProps) => {
   const [sum, setSum] = useState(0);
   const pathname = usePathname();
   const confetti = useConfettiStore();
-  console.log(confetti);
   const router = useRouter();
   const { examenId } = params;
 
-  // const { data, isPending } = useQuery<any>({
-  //   queryKey: ['examenById', examenId],
-  //   queryFn: async () => await getOneExamById({ id: examenId }),
-  // });
   const queryClient = useQueryClient();
-  const data = queryClient.getQueryData(['examenById', examenId]) as any;
+  const { data, isLoading } = useQuery<any>(
+    ['examenById', examenId],
+    async () => await getOneExamById({ id: examenId }),
+    {
+      initialData: queryClient.getQueryData(['examenById', examenId]),
+    }
+  );
   const { editExam, isPending: isPendingEdit } = useEditExamContent();
 
   const [fakeData, setFakeData] = useState<any>([]);
@@ -42,15 +49,16 @@ export default function Page({ params }: { params: { examenId: string; etab_id: 
     setSum(calcAllMark(fakeData));
   }, [fakeData]);
 
-  // if (isPending) return <Loading />;
+  const isSavingDisabled = fakeData === data?.content || isPendingEdit;
 
-  function handleCancel() {
+  const handleCancel = () => {
     router.back();
-  }
-  const arabic = data?.language === 'ar' ? true : false;
+  };
+
+  const isArabic = data?.language === 'ar' ? true : false;
+
   const haveZeroInfakeData = (data: any) => {
     for (const item of data) {
-      console.log(item.mark);
       if (item.mark === 0) {
         return true;
       }
@@ -60,70 +68,61 @@ export default function Page({ params }: { params: { examenId: string; etab_id: 
     }
     return false;
   };
+
   const handleSaveData = () => {
-    if (fakeData === data?.content) {
+    if (isSavingDisabled) {
       toast.error("Aucune modification n'a été effectuee.");
       return;
     }
+
     if (sum > data?.total_mark) {
       toast.error(`La note doit être minimum a ${data?.total_mark}`);
       return;
     }
+
     if (haveZeroInfakeData(fakeData)) {
       toast.error('Ilya des champs obligatoires sont manquants.');
       return;
     }
-    let exam_id = examenId;
-    console.log(fakeData);
-    editExam({ exam_id, content: fakeData });
-    confetti.onOpen();
 
-    handleConfetti;
-  };
-  // useEffect(() => {
-  //   if (fakeData) {
-  //   }
-  // }, [fakeData]);
-  const handleConfetti = () => {
+    editExam({ exam_id: examenId, content: fakeData });
     confetti.onOpen();
   };
+
   return (
     <div className="flex flex-col gap-6 p-10 overflow-auto">
       <nav className="flex justify-between w-full ">
         <div className="flex flex-col gap-4">
-          {/* {isPending ? (
+          {isLoading ? (
             <Skeleton className="w-[300px] h-[40px]" />
-          ) : ( */}
-          <div className="text-[#1B8392] text-2xl font-semibold ">{data?.name} </div>
-          {/* )} */}
-          <div className="flex items-center text-[#727272]">
-            <Image src="/arrowleft.svg" alt="icons" width={20} height={20} />
-            <span className="cursor-pointer">Examens</span>
-            <Image src="/arrowleft.svg" alt="icons" width={20} height={20} />
-            {/* {isPending ? (
-              <span className="flex items-center gap-3 cursor-pointer">
-                <Skeleton className="w-[80px] h-[20px]" />
-                <Skeleton className="w-[80px] h-[20px]" />
-                <Skeleton className="w-[80px] h-[20px]" />
-              </span>
-            ) : ( */}
-            <div className="flex gap-1 cursor-pointer">
-              {data?.exam_classess.map((item: any) => (
-                <span onClick={() => router.push(`/${params?.etab_id}/classes/${item.id}`)}>
-                  {item.name} ,
-                </span>
-              ))}
-            </div>
-            {/* )} */}
-          </div>
+          ) : (
+            <>
+              <div className="text-[#1B8392] text-2xl font-semibold ">{data?.name} </div>
+              <div className="flex items-center text-[#727272]">
+                <Image src="/arrowleft.svg" alt="icons" width={20} height={20} />
+                <span className="cursor-pointer">Examens</span>
+                <Image src="/arrowleft.svg" alt="icons" width={20} height={20} />
+                <div className="flex gap-1 cursor-pointer">
+                  {data?.exam_classess.map((item: any) => (
+                    <span
+                      onClick={() => router.push(`/${params?.etab_id}/classes/${item.id}`)}
+                      key={item.id}
+                    >
+                      {item.name} ,
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </>
+          )}
         </div>
 
         <div className="flex gap-3 pt-4 h-14 cursor-pointe ">
           <div
             className={cn(
               'flex items-center w-[109px]  gap-3  border rounded-lg cursor-pointer border-[#F04438]  text-[rgb(240,68,56)] hover:opacity-80',
-              +sum < +data?.total_mark && 'bg-[#dea53b1b] border-[#dea53b] text-[#dea53b] ',
-              +sum === +data?.total_mark && 'bg-[#d1fadf] border-[#399739] text-[#399739] '
+              sum < data?.total_mark && 'bg-[#dea53b1b] border-[#dea53b] text-[#dea53b] ',
+              sum === data?.total_mark && 'bg-[#d1fadf] border-[#399739] text-[#399739] '
             )}
           >
             <button
@@ -134,38 +133,5 @@ export default function Page({ params }: { params: { examenId: string; etab_id: 
               {sum} / {data?.total_mark}
             </button>
           </div>
-          <div className="flex items-center gap-3 p-2 border rounded-lg cursor-pointer border-[#F04438] text-[#F04438] hover:opacity-80 ">
-            <button
-              onClick={() => handleCancel()}
-              className="flex items-center gap-3 pl-2 pr-2 text-sm font-semibold leading-tight text-center"
-            >
-              <Image src="/redcloseicon2.svg" alt="icons" width={10} height={10} />
-              Annuler
-            </button>
-          </div>
-
-          <div className="flex items-center p-2 border rounded-lg cursor-pointer bg-[#1B8392] text-white gap-3 hover:opacity-80 ">
-            <div className="flex items-center gap-3 pl-2 pr-2 text-sm font-semibold leading-tight text-center">
-              <Image src="/importerIcon.svg" alt="icons" width={20} height={20} />
-              Importer
-            </div>
-          </div>
-
           <div
-            className="flex items-center p-2 border rounded-lg cursor-pointer bg-[#1B8392] text-white gap-3 hover:opacity-80 "
-            onClick={handleSaveData}
-          >
-            <div className="flex items-center gap-3 pl-2 pr-2 text-sm font-semibold leading-tight text-center">
-              <Image src={'/enregistreIcon.svg'} alt="icons" width={20} height={20} className="" />
-              Enregister
-            </div>
-          </div>
-        </div>
-      </nav>
-      {/* <CreateExam examId={examenId} /> */}
-      {/* <EmailSend /> */}
-
-      <CreateExam data={data} isArabic={arabic} setFakeData={setFakeData} fakeData={fakeData} />
-    </div>
-  );
-}
+            className="flex items-center gap-3 p-2 border rounded-lg cursor-pointer border-[#F04438] text-[#F04438] hover:opacity-
