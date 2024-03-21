016ff1597a751cf4422680c04b6f185ed38e7b14
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:32:58 +0100

    improve

--- app/@teacher/[etab_id]/(teacherdashboard)/(routes)/calendrier/sections/eventSchema.ts
+++ app/@teacher/[etab_id]/(teacherdashboard)/(routes)/calendrier/sections/eventSchema.ts
@@ -3,13 +3,13 @@ import * as Yup from 'yup';
 const useValidationsCategorySchema = () => {
   const validationsCategorySchema = Yup.object().shape({
     title: Yup.string()
-      .max(100, 'tu ne peux pas transmettre 100 caractères')
+      .max(100, 'Vous ne pouvez pas transmettre plus de 100 caractères')
       .required('Le nom est requis'),
-    start: Yup.date().required('la date de début est requise'),
-    end: Yup.date().required('la date de fin est requise'),
-    color: Yup.object().required('la couleur est obligatoire'),
-    classes: Yup.array().required('le classe est obligatoire').min(1, 'le classe est obligatoire'),
-    subject: Yup.object().required('la matière est obligatoire'),
+    start: Yup.date().required('La date de début est requise'),
+    end: Yup.date().required('La date de fin est requise'),
+    color: Yup.object().required('La couleur est obligatoire'),
+    classes: Yup.array().of(Yup.string()).required('Au moins une classe est requise').min(1, 'Une classe est requise'),
+    subject: Yup.object().required('La matière est obligatoire'),
     description: Yup.string(),
     studentsVisibility: Yup.boolean().required(),
     establishment: Yup.string(),
