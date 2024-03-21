b4f2058b9ea331042270ac9e3eb1aa13456d8854
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:33:02 +0100

    improve

--- app/@teacher/[etab_id]/(teacherdashboard)/_components/SettingsBtn.tsx
+++ app/@teacher/[etab_id]/(teacherdashboard)/_components/SettingsBtn.tsx
@@ -4,35 +4,38 @@ import { cn } from '@/lib/utils';
 import { useState } from 'react';
 import { boolean } from 'zod';
 
-interface settingBtnProps {
+type SettingBtnProps = {
   onClick: () => void;
   isActive: boolean;
-}
-function SettingsBtn({ onClick, isActive }: settingBtnProps) {
+};
+
+type CnProps = {
+  active: string;
+  inactiveHover: string;
+  inactive: string;
+};
+
+const cnProps = {
+  active: 'w-full text-mainGreen rounded-lg',
+  inactiveHover: 'hover:bg-slate-300/20  hover:rounded-lg duration-300',
+  inactive: 'w-full text-slate-500 text-sm font-[400] pl-3 transition-all',
+};
+
+function SettingsBtn({ onClick, isActive }: SettingBtnProps) {
   const { collapsed } = useSidebar((state) => state);
 
   return (
     <p
       className={cn(
-        'flex items-start w-full  text-slate-500 text-sm font-[400] pl-3 transition-all',
-        !isActive && 'hover:bg-slate-300/20  hover:rounded-lg duration-300',
-        isActive && ' w-full text-mainGreen rounded-lg'
+        cnProps.inactive,
+        isActive ? cnProps.active : cnProps.inactiveHover
       )}
     >
       <span className={cn('flex items-center w-full text-lg w gap-x-3 p-1')}>
         <Image
           src={isActive ? '/settings-fill.svg' : '/settings.svg'}
-          // src={'/icons/kebab-menu.svg'}
-          alt="settings icon"
+          alt=""
+          title={isActive ? 'Active settings icon' : 'Inactive settings icon'}
           width={collapsed ? 20 : 20}
           height={collapsed ? 20 : 20}
-          className={cn('relative ', isActive && 'text-white  ')}
-        />
-
-        {collapsed ? '' : <span className=' font-[400]'>Paramètres</span>}
-      </span>
-    </p>
-  );
-}
-
-export default SettingsBtn;
+          className={cn('relative
