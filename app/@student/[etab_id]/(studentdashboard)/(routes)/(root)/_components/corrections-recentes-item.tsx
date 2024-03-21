e114dabbd0f7952f1aeb6579d068da29cde90c7a
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:32:54 +0100

    improve

--- app/@student/[etab_id]/(studentdashboard)/(routes)/(root)/_components/corrections-recentes-item.tsx
+++ app/@student/[etab_id]/(studentdashboard)/(routes)/(root)/_components/corrections-recentes-item.tsx
@@ -10,54 +10,55 @@ import ExamCorrectionList from './exam-correction-list';
 import Rien from './Rien';
 import BulletinsStudentList from './bulletins-student-list';
 
-// const CorrectionsRecentes = ({ data }: any) => {
-//   return (
-//     <>
-//       {!data ? (
-//         <div className="w-full border h-full rounded-xl flex items-center justify-center p-10">
-//           <Rien
-//             image="/dashboard/bag.svg"
-//             className="flex flex-col gap-6 justify-center"
-//             message="Pas d'examens pour le moment"
-//           />
-//         </div>
-//       ) : (
-//         <div className="flex flex-col gap-4">
-//           <div className="flex items-center justify-end gap-3">
-//             {/* Devoir */}
-//             <Select>
-//               <SelectTrigger className=" w-1/4 rounded-xl  text-11">
-//                 <SelectValue className="text-sm" placeholder="les plus récents" />
-//               </SelectTrigger>
-//               <SelectContent>
-//                 <SelectItem value="light">les plus récents</SelectItem>
-//                 <SelectItem value="light">les plus elevée</SelectItem>
-//               </SelectContent>
-//             </Select>
-//           </div>
-//           <div className="w-full border h-[300px] rounded-xl overflow-auto">
-//           <BulletinsStudentList />
-           
-//           </div>
-//         </div>
-//       )}
-//     </>
-//   );
-// };
+type CorrectionsRecentesProps = {
+  data?: any;
+  isLoading: boolean;
+};
 
-// export default CorrectionsRecentes;
+function CorrectionsRecentes({ data, isLoading }: CorrectionsRecentesProps) {
+  if (isLoading) {
+    return (
+      <div className="w-full border h-full rounded-xl flex items-center justify-center p-10">
+        <Rien
+          image="/dashboard/bag.svg"
+          className="flex flex-col gap-6 justify-center"
+          message="Chargement des examens..."
+        />
+      </div>
+    );
+  }
 
-function CorrectionsRecentes() {
   return (
-    <div className="w-full border h-full rounded-xl flex items-center justify-center p-10">
-      <Rien
-        image="/dashboard/bag.svg"
-        className="flex flex-col gap-6 justify-center"
-        message="Pas d'examens pour le moment"
-      />
-    </div>
+    <>
+      {!data ? (
+        <div className="w-full border h-full rounded-xl flex items-center justify-center p-10">
+          <Rien
+            image="/dashboard/bag.svg"
+            className="flex flex-col gap-6 justify-center"
+            message="Pas d'examens pour le moment"
+          />
+        </div>
+      ) : (
+        <div className="flex flex-col gap-4">
+          <div className="flex items-center justify-end gap-3">
+            {/* Devoir */}
+            {/* <Select>
+              <SelectTrigger className=" w-1/4 rounded-xl  text-11">
+                <SelectValue className="text-sm" placeholder="les plus récents" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="light">les plus récents</SelectItem>
+                <SelectItem value="light">les plus elevée</SelectItem>
+              </SelectContent>
+            </Select> */}
+          </div>
+          <div className="w-full border h-[300px] rounded-xl overflow-auto">
+            <ExamCorrectionList data={data} />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
 export default CorrectionsRecentes;
-
