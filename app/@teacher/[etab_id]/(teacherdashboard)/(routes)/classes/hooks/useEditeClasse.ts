f20b76a121fd911b8b6c1bde5bff70bc655b6cad
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:33:01 +0100

    improve

--- app/@teacher/[etab_id]/(teacherdashboard)/(routes)/classes/hooks/useEditeClasse.ts
+++ app/@teacher/[etab_id]/(teacherdashboard)/(routes)/classes/hooks/useEditeClasse.ts
@@ -1,32 +1,39 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-
+import { UseMutationResult, UseQueryClient } from '@tanstack/react-query';
 import { toast } from 'react-hot-toast';
 import { updateClasse as updateClasseApi } from '@/actions/classe';
 
-interface editeClasseParams {
+type EditClasseParams = {
   name: string;
   classe_id: number;
   matiere: string; // Update with the actual type for class data
-}
+};
 
-export function useEditeClasse() {
-  const queryClient = useQueryClient();
-  const { mutate: editeClass, isPending } = useMutation({
-    mutationFn: ({ name, classe_id, matiere }: editeClasseParams) =>
+export function useEditClasse(
+  queryClient: UseQueryClient
+): UseMutationResult<void, Error, EditClasseParams> {
+  return useMutation(
+    ({ name, classe_id, matiere }: EditClasseParams) =>
       updateClasseApi(name, classe_id, matiere),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['classe'] });
-      queryClient.invalidateQueries({ queryKey: ['dashClasses'] });
-      queryClient.invalidateQueries({ queryKey: ['dashClasseCount'] });
-      queryClient.invalidateQueries({ queryKey: ['dashStudentClasses'] });
-      toast.success('Classe modifiée avec succès.');
-    },
-    onError: (err) => {
-      console.error('Error edite class:', err);
-      toast.error('Erreur lors de la modification de la classe.');
-    },
-    retry: false,
-  });
+    {
+      onSuccess: () => {
+        const queriesToInvalidate = [
+          'classe',
+          'dashClasses',
+          'dashClasseCount',
+          'dashStudentClasses',
+        ];
 
-  return { editeClass, isPending };
+        queriesToInvalidate.forEach((queryKey) => {
+          queryClient.invalidateQueries({ queryKey });
+        });
+
+        toast.success('Classe modifiée avec succès.');
+      },
+      onError: (err) => {
+        console.error('Error edite class:', err);
+        toast.error('Erreur lors de la modification de la classe.');
+      },
+      retry: false,
+    }
+  );
 }
