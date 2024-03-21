6e3aefc4e4e9473277025133f3c5ea6809b4601c
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:33:07 +0100

    improve

--- data/password-reset-token.ts
+++ data/password-reset-token.ts
@@ -1,22 +1,26 @@
 import { db } from '@/lib/db';
-export const getPasswordResetTokenByToken = async (token: string) => {
+
+export const getPasswordResetTokenByToken = async (token: string): Promise<typeof db.passwordResetToken | null> => {
   try {
     const passwordToken = await db.passwordResetToken.findUnique({
       where: { token },
     });
     return passwordToken;
-  } catch {
+  } catch (error) {
+    console.error('Error while fetching password reset token by token:', error);
     return null;
   }
 };
 
-export const getPasswordResetTokenByEmail = async (email: string) => {
+export const getPasswordResetTokenByEmail = async (email: string): Promise<typeof db.passwordResetToken | null> => {
   try {
     const passwordToken = await db.passwordResetToken.findFirst({
       where: { email },
     });
     return passwordToken;
-  } catch {
+  } catch (error) {
+    console.error('Error while fetching password reset token by email:', error);
     return null;
   }
 };
+
