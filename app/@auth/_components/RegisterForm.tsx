45c4a67bf7029677489f12134c4d39aa88fc419d
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:32:46 +0100

    improve

--- app/@auth/_components/RegisterForm.tsx
+++ app/@auth/_components/RegisterForm.tsx
@@ -5,12 +5,14 @@ import ProfForm from './ProfForm';
 import { useState } from 'react';
 export default function RegisterForm() {
   const [role, setRole] = useState<string>('TEACHER');
+
   const handleRole = (role: string) => {
     setRole(role);
   };
-  if (role === 'TEACHER') {
-    return <ProfForm handleRole={handleRole} />;
-  } else {
-    return <EtudiantForm handleRole={handleRole} />;
-  }
+
+  return (
+    <div>
+      {role === 'TEACHER' ? <ProfForm handleRole={handleRole} /> : <EtudiantForm handleRole={handleRole} />}
+    </div>
+  );
 }
