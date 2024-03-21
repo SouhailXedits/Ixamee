92ba54920e76a069578d0a495d1139e48964559f
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:33:19 +0100

    improve

--- app/@teacher/[etab_id]/(teacherdashboard)/(routes)/examens/[examenId]/_components/CreateExercice.tsx
+++ app/@teacher/[etab_id]/(teacherdashboard)/(routes)/examens/[examenId]/_components/CreateExercice.tsx
@@ -4,53 +4,53 @@ import { CreateQuestion } from './CreateQuestion';
 import { cn } from '@/lib/utils';
 import { calculerExerciceMark } from '@/app/_utils/calculateChildrenMarks';
 
-export const CreateExercice = ({ allData, data, setFakeData, isArabic }: any) => {
-  if (!data) return;
+interface CreateExerciceProps {
+  allData: any[];
+  data: any;
+  setFakeData: (data: any[]) => void;
+  isArabic: boolean;
+}
 
-  // // const isArabic = language === 'ar';
-  // const isArabic = false;
+export const CreateExercice: React.FC<CreateExerciceProps> = ({
+  allData,
+  data,
+  setFakeData,
+  isArabic,
+}) => {
+  if (!data) return null;
+
   const AddQuestion = () => {
-    setFakeData((prevData: any) => {
-      const getNextChar = (num: number) => String.fromCharCode(0x2160 + num);
+    const getNextChar = (num: number) => String.fromCharCode(0x2160 + num);
 
-      const newData = {
-        id: Math.random().toString(36).substring(7),
-        name: getNextChar(data.children.length),
-        mark: 1,
-        content: '',
-        children: [],
-      };
+    const newData = {
+      id: Math.random().toString(36).substring(7),
+      name: getNextChar(data.children.length),
+      mark: 1,
+      content: '',
+      children: [],
+    };
 
-      const updatedData = prevData.map((item: any) => {
-        if (item.id === data.id) {
-          return { ...item, children: [...item.children, newData] };
-        }
-        return item;
-      });
-
-      return updatedData;
-    });
+    setFakeData((prevData: any[]) =>
+      prevData.map((item: any) =>
+        item.id === data.id ? { ...item, children: [...item.children, newData] } : item
+      )
+    );
   };
+
   const deleteExercice = (data: any) => {
-    const newData = allData.filter((item: any) => item.id !== data.id);
-    setFakeData(newData);
+    setFakeData(allData.filter((item: any) => item.id !== data.id));
   };
 
-  //   calculateChildrenMarks(allData);  console.log(allData)
-  const hndelChangeExcericeName = (e: any) => {
-    setFakeData((prevData: any) => {
-      return prevData.map((item: any) => {
-        if (item.id === data.id) {
-          return { ...item, name: e.target.value };
-        }
-        return item;
-      });
-    });
+  const handleChangeExcericeName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFakeData((prevData: any[]) =>
+      prevData.map((item: any) =>
+        item.id === data.id ? { ...item, name: e.target.value } : item
+      )
+    );
   };
 
   return (
     <div
-      // key={}
       className="flex flex-col border border-[#D9D9D9] rounded-lg p-2 min-h-[200px] pb-16"
     >
       <div className="flex items-center justify-between w-full p-3">
@@ -62,10 +62,8 @@ export const CreateExercice = ({ allData, data, setFakeData, isArabic }: any) =>
                 'text-2xl bg-transparent border-none -mr-10 -ml-1 text-[#1B8392]',
                 isArabic && 'pr-[3rem]'
               )}
-              // defaultValue={data.name || !isArabic ? 'Exercice 1' : 'تمرين 1'}
-              // defaultValue={data.name}
               value={data.name}
-              onChange={(e) => hndelChangeExcericeName(e)}
+              onChange={handleChangeExcericeName}
             />
 
             <Image
@@ -73,11 +71,10 @@ export const CreateExercice = ({ allData, data, setFakeData, isArabic }: any) =>
               width={24}
               height={24}
               alt="editicon"
-              // onClick={}
               className="cursor-pointer"
             />
           </div>
-          {}
+
           <div className="flex items-center gap-3 ">
             <div
               className={cn(
@@ -93,7 +90,6 @@ export const CreateExercice = ({ allData, data, setFakeData, isArabic }: any) =>
                   maxLength={5}
                   disabled
                   value={Number(calculerExerciceMark(data)).toFixed(2)}
-                  // defaultValue={}
                 />
                 {/* <span className="text-xl">/ 20.00</span> */}
               </div>
@@ -113,9 +109,6 @@ export const CreateExercice = ({ allData, data, setFakeData, isArabic }: any) =>
 
       <div className="flex flex-col gap-6 ml-10">
         <div className="flex flex-col gap-4 ">
-          {/* {isArabic && (
-            <span className="pr-10 -mb-4 text-xl pt-2 text-[#D9D9D9]">اكتب النص هنا</span>
-          )} */}
           {data.children.map((item: any) => (
             <CreateQuestion
               key={item.id}
@@ -130,7 +123,7 @@ export const CreateExercice = ({ allData, data, setFakeData, isArabic }: any) =>
         <div className="px-4 pt-4 -mb-10">
           <div
             className="bg-[#1B8392] w-52 flex items-center text-white rounded-lg p-2 cursor-pointer justify-center  "
-            onClick={() => AddQuestion()}
+            onClick={AddQuestion}
           >
             <Image src="/plusicon.svg" width={20} height={20} alt="plusicon" />
             {!isArabic ? 'Ajoutez une question' : 'إضافة سؤال'}
