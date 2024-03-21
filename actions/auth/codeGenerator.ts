7fb85fa3b3a422e55c2896446ada04056793b4d7
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:32:48 +0100

    improve

--- actions/auth/codeGenerator.ts
+++ actions/auth/codeGenerator.ts
@@ -1,3 +1,10 @@
 export function generateSixDigitNumber() {
-    return Math.floor(100000 + Math.random() * 900000);
-  }
+  // Generate a random number between 0 and 1
+  const randomValue = Math.random();
+
+  // Multiply the random number by 10 million (9,999,999) to shift the range to six digits
+  const sixDigitNumber = Math.floor(randomValue * 9_000_000);
+
+  // Add 100,000 to shift the range to start from 100,000 instead of 0
+  return 100_000 + sixDigitNumber;
+}
