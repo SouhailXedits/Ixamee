6767643ac5da5725a84b58144b53ca448440dad5
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:33:08 +0100

    improve

--- app/@student/[etab_id]/(studentdashboard)/(routes)/pdf/components/Table.tsx
+++ app/@student/[etab_id]/(studentdashboard)/(routes)/pdf/components/Table.tsx
@@ -1,4 +1,50 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const css = styled.css`
+  display: inline-flex;
+  flex-direction: column;
+  align-items: flex-start;
+  border: 1px solid #ccc;
+`;
+
+const StudentCell = styled.div`
+  display: flex;
+  width: 125fr;
+  height: 46px;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  padding: 0px 0px 0px 5px;
+  background-color: #f2f2f2;
+  border: 1px solid #ccc;
+`;
+
+const GradeCell = styled.div`
+  display: flex;
+  width: 145fr;
+  height: 22px;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  background-color: ${(props) => (props.index % 2 === 0 ? '#f9f9f9' : '#fff')};
+  border: 1px solid #ccc;
+`;
+
+const GradeNoteCell = styled.div`
+  flex: 1;
+  align-self: stretch;
+  margin-top: -0.25px;
+  font-family: Poppins-Regular;
+  font-weight: 400;
+  color: #000;
+  font-size: 10px;
+  text-align: center;
+  letter-spacing: 0;
+  line-height: 18px;
+`;
+
+const GradeRankCell = styled(GradeNoteCell)``;
 
 const fakeData = [
   {
@@ -12,122 +58,66 @@ const fakeData = [
   // Add more fake data as needed
 ];
 
-const Table = () => (
-  <div
-    style={{
-      display: 'inline-flex',
-      flexDirection: 'column',
-      alignItems: 'flex-start',
-      border: '1px solid #ccc',
-    }}
-  >
-    {fakeData.map((studentData :any, index:number) => (
-      <div
-        key={index}
-        style={{
-          display: 'inline-flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <div
-          style={{
-            display: 'flex',
-            width: '125px',
-            height: '46px',
-            alignItems: 'center',
-            justifyContent: 'center',
-            gap: '10px',
-            padding: '0px 0px 0px 5px',
-            backgroundColor: '#f2f2f2',
-            border: '1px solid #ccc',
-          }}
-        >
-          <div
-            style={{
-              flex: 1,
-              alignSelf: 'stretch',
-              marginTop: '-0.5px',
-              fontFamily: 'Poppins-Regular',
-              fontWeight: 400,
-              color: '#000',
-              fontSize: '12px',
-              letterSpacing: 0,
-              lineHeight: '18px',
-            }}
-          >
-            {studentData.student}
-          </div>
-        </div>
-        {studentData.grades.map((grade :any, gradeIndex :any) => (
-          <div
-            key={gradeIndex}
-            style={{
-              display: 'flex',
-              width: '145px',
-              height: '22px',
-              alignItems: 'center',
-              justifyContent: 'center',
-              position: 'relative',
-              backgroundColor: gradeIndex % 2 === 0 ? '#f9f9f9' : '#fff',
-              border: '1px solid #ccc',
-            }}
-          >
-            <div
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                gap: '10px',
-                flex: 1,
-                alignSelf: 'stretch',
-                flexGrow: 1,
-                marginTop: '-1px',
-                marginBottom: '-1px',
-                marginLeft: '-1px',
-                border: '0.5px solid #ccc',
-              }}
-            >
-              <div
-                style={{
-                  flex: 1,
-                  alignSelf: 'stretch',
-                  marginTop: '-0.25px',
-                  fontFamily: 'Poppins-Regular',
-                  fontWeight: 400,
-                  color: '#000',
-                  fontSize: '10px',
-                  textAlign: 'center',
-                  letterSpacing: 0,
-                  lineHeight: '18px',
-                }}
-              >
-                {grade.note}
-              </div>
-              <div
-                style={{
-                  flex: 1,
-                  alignSelf: 'stretch',
-                  marginTop: '-0.25px',
-                  fontFamily: 'Poppins-Regular',
-                  fontWeight: 400,
-                  color: '#000',
-                  fontSize: '10px',
-                  textAlign: 'center',
-                  letterSpacing: 0,
-                  lineHeight: '18px',
-                }}
-              >
-                {grade.rank}
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    ))}
-  </div>
+interface TableProps {
+  data: typeof fakeData[0];
+}
 
+const Table: React.FC<TableProps> = ({ data }) => (
+  <css>
+    {data.map((studentData, index) => (
+      <StudentCell key={index}>
+        <div>{studentData.student}</div>
+        {studentData.grades.map((grade, gradeIndex) => (
+          <GradeCell key={gradeIndex} index={gradeIndex}>
+            <GradeNoteCell>{grade.note}</GradeNoteCell>
+            <GradeRankCell>{grade.rank}</GradeRankCell>
+          </GradeCell>
+        ))}
+      </StudentCell>
+    ))}
+  </css>
 );
 
 export default Table;
+
+
+npm install --save styled-components
+
+
+import styled from 'styled-components';
+
+
+const StudentCell = styled.div`
+  display: flex;
+  width: 125fr;
+  height: 46px;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  padding: 0px 0px 0px 5px;
+  background-color: #f2f2f2;
+  border: 1px solid #ccc;
+`;
+
+
+<StudentCell>
+  <div>{studentData.student}</div>
+</StudentCell>
+
+
+const GradeCell = styled.div<{index: number}>`
+  display: flex;
+  width: 145fr;
+  height: 22px;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  background-color: ${(props) => (props.index % 2 === 0 ? '#f9f9f9' : '#fff')};
+  border: 1px solid #ccc;
+`;
+
+
+<GradeCell key={gradeIndex} index={gradeIndex}>
+  <GradeNoteCell>{grade.note}</GradeNoteCell>
+  <GradeRankCell>{grade.rank}</GradeRankCell>
+</GradeCell>
