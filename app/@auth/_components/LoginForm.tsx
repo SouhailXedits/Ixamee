f3dbacfabc60ec2c0af2cb468cc298e016871a90
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:33:32 +0100

    improve

--- app/@auth/_components/LoginForm.tsx
+++ app/@auth/_components/LoginForm.tsx
@@ -1,7 +1,7 @@
-'use client';
-import * as z from 'zod';
+import { useRouter } from 'next/router';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
 import { Checkbox } from '@/components/ui/auth-checkbox';
 import {
   Form,
@@ -11,29 +11,49 @@ import {
   FormLabel,
   FormMessage,
 } from '@/components/ui/form';
-import { LoginSchema } from '@/actions/auth/schemas';
 import { Input } from '@/components/ui/auth-input';
 import { Button } from '@/components/ui/button';
 import FormError from '@/components/ui/form-error';
 import FormSuccess from '@/components/ui/form-success';
-import { login } from '@/actions/auth/login';
-import { useCallback, useEffect, useState, useTransition } from 'react';
+import { login, sendEmailVerificationToken } from '@/actions/auth/login';
+import { useCallback, useEffect, useState } from 'react';
 import Link from 'next/link';
 import { MdOutlineEmail } from 'react-icons/md';
 import { IoKeyOutline } from 'react-icons/io5';
 import bcryptjs from 'bcryptjs';
 import { generateSixDigitNumber } from '@/actions/auth/codeGenerator';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { sendEmailVerificationToken } from '@/actions/auth/sendEmailVerificationToken';
+import { useSearchParams } from 'next/navigation';
+
+const LoginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+  rememberMe: z.boolean(),
+});
 
 export default function LoginForm() {
-  const [error, setError] = useState<string | undefined>('');
-  const [success, setSuccess] = useState<string | undefined>('');
   const router = useRouter();
-
   const searchParams = useSearchParams();
   const errorAuth = searchParams.get('error');
-  const onEnter = useCallback(async () => {
+
+  const [error, setError] = useState<string | undefined>('');
+  const [success, setSuccess] = useState<string | undefined>('');
+  const [isRegistrationFormSuccessful, setRegistrationFormSuccessful] = useState<boolean>(false);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+      rememberMe: false,
+    },
+  });
+
+  const onEnter = useCallback(() => {
     if (errorAuth) {
       setError('E-mail déjà utilisée');
     }
@@ -43,29 +63,18 @@ export default function LoginForm() {
     onEnter();
   }, [onEnter]);
 
-  const form = useForm<z.infer<typeof LoginSchema>>({
-    resolver: zodResolver(LoginSchema),
-    defaultValues: {
-      email: '',
-      password: '',
-      rememberMe: false,
-    },
-  });
-
-  const [showPassword, setShowPassword] = useState(false);
-  const [isRegistrationFormSuccessful, setRegistrationFormSuccessful] = useState<boolean>(false);
-
-  const [isPending, startTransition] = useTransition();
-
   const onSubmit = (values: z.infer<typeof LoginSchema>) => {
     setError('');
     setSuccess('');
-    startTransition(async () => {
-      let code = generateSixDigitNumber();
-      const hashedCode = await bcryptjs.hash(code + '', 10);
-      login(values, code).then((data) => {
+
+    const code = generateSixDigitNumber();
+    const hashedCode = bcryptjs.hashSync(code, 10);
+
+    login(values, hashedCode)
+      .then((data) => {
         setError(data?.error);
         setSuccess(data?.success);
+
         if (data?.success === 'Un e-mail a été envoyé ! Veuillez vérifier votre compte.') {
           localStorage.setItem(
             'new-verification',
@@ -79,6 +88,7 @@ export default function LoginForm() {
           );
           sendEmailVerificationToken(values.email);
         }
+
         if (data?.success === 'Vous étes presque arrivé ! complete votre inscription') {
           localStorage.setItem(
             'new-verification',
@@ -91,8 +101,10 @@ export default function LoginForm() {
           );
           setRegistrationFormSuccessful(true);
         }
+      })
+      .catch((error) => {
+        setError(error.message);
       });
-    });
   };
 
   useEffect(() => {
@@ -102,14 +114,13 @@ export default function LoginForm() {
   }, [isRegistrationFormSuccessful, router]);
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6" method="post">
+    <Form>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-6" method="post">
         <FormError message={error} />
         <FormSuccess message={success} />
         <div className="w-full flex flex-col gap-4">
           <FormField
-            control={form.control}
-            name="email"
+            control={{ ...register('email') }}
             render={({ field }) => (
               <FormItem>
                 <FormLabel>E-mail</FormLabel>
@@ -120,7 +131,6 @@ export default function LoginForm() {
                     name="e-mail"
                     type="email"
                     icon={<MdOutlineEmail className="text-muted-foreground w-5 h-5" />}
-                    disabled={isPending}
                   />
                 </FormControl>
                 <FormMessage />
@@ -128,8 +138,7 @@ export default function LoginForm() {
             )}
           />
           <FormField
-            control={form.control}
-            name="password"
+            control={{ ...register('password') }}
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Mot de passe</FormLabel>
@@ -140,7 +149,6 @@ export default function LoginForm() {
                     name="mot de passe"
                     type={showPassword ? 'text' : 'password'}
                     icon={<IoKeyOutline className="text-muted-foreground w-5 h-5" />}
-                    disabled={isPending}
                   />
                 </FormControl>
                 <FormMessage />
@@ -150,49 +158,10 @@ export default function LoginForm() {
         </div>
         <div className="flex items-center">
           <FormField
-            control={form.control}
-            name="rememberMe"
+            control={{ ...register('rememberMe') }}
             render={({ field }) => (
               <FormLabel className="flex w-full justify-between">
                 <div className="flex items-center ">
                   <Checkbox
                     className="mr-1 checked:bg-transparent border-gray"
-                    checked={field.value}
-                    onCheckedChange={field.onChange}
-                    name="rester connecté"
-                    disabled={isPending}
-                  />
-                  <label
-                    htmlFor="terms"
-                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                  >
-                    Rester connecté
-                  </label>
-                </div>
-                <Button
-                  size="sm"
-                  variant="link"
-                  asChild
-                  className="text-center text-mainGreen hover:underline text-sm font-semibold max-md:text-xs"
-                  name="btn"
-                >
-                  <Link href={'/reset-password'}>Mot de passe oublié?</Link>
-                </Button>
-              </FormLabel>
-            )}
-          />
-        </div>
-        <Button
-          name="submitButton"
-          type="submit"
-          disabled={isPending}
-          className={`${form.formState.isValid ? 'bg-2' : 'bg-12'} ${
-            isPending ? ' animate-bounce-text-opacity  ' : ''
-          }  w-full h-10 pt-2  font-semibold items-start justify-center rounded-lg text-center text-white text-base hover:opacity-75`}
-        >
-          {isPending ? 'Connexion en cours...' : ' Se connecter'}
-        </Button>
-      </form>
-    </Form>
-  );
-}
+                   
