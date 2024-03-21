65fcba1ea4bee8a93cb40f3fff2bc0d8f9f27b7f
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:32:54 +0100

    improve

--- app/@teacher/[etab_id]/(teacherdashboard)/(routes)/calendrier/sections/index.tsx
+++ app/@teacher/[etab_id]/(teacherdashboard)/(routes)/calendrier/sections/index.tsx
@@ -1,4 +1,13 @@
-export { default as StyledCalendar } from './styles';
-export { default as CalendarForm } from './CalendarForm';
-export { default as CalendarToolbar } from './CalendarToolbar';
-// export { default as CalendarFilterDrawer } from './CalendarFilterDrawer';
+// src/components/Calendar/index.js
+
+import StyledCalendar from './styles';
+import CalendarForm from './CalendarForm';
+import CalendarToolbar from './CalendarToolbar';
+import CalendarFilterDrawer from './CalendarFilterDrawer';
+
+// Export all components as the default export
+export { StyledCalendar as default, CalendarForm, CalendarToolbar, CalendarFilterDrawer };
+
+
+// Another file importing the Calendar components
+import { CalendarForm, CalendarToolbar } from './components/Calendar';
