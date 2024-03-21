6cd568668cede05fd34a52c4690ed8da6dbd8624
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:33:04 +0100

    improve

--- components/modals/drop-down.tsx
+++ components/modals/drop-down.tsx
@@ -1,27 +1,21 @@
-import { Button } from '@/components/ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuLabel,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import { ModifierUneClasse } from './modifier-une-classe';
-import { SupprimerUneClasse } from './suprimer-classe';
-import { ArchiveUneClasse } from './archiver-classe';
+import { Button, DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuLabel, DropdownMenuTrigger } from '@/components/ui';
+import ModifierUneClasse from './modifier-une-classe';
+import SupprimerUneClasse from './suprimer-classe';
+import ArchiveUneClasse from './archiver-classe';
 import { useState } from 'react';
+
 interface DropdownMenuItemSelectProps {
   children: React.ReactNode;
   data: any;
 }
 
-export const DropdownMenuItemSelect = ({ children, data }: DropdownMenuItemSelectProps) => {
-  const [isopen, setIsOpen] =useState(false)
+const DropdownMenuItemSelect = ({ children, data }: DropdownMenuItemSelectProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    
-    <DropdownMenu modal={isopen}>
+    <DropdownMenu open={isOpen} onOpenChange={setIsOpen}>
       <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
-      <DropdownMenuContent className="w-45  text-[#727272]">
+      <DropdownMenuContent className="w-45 text-[#727272]">
         <DropdownMenuGroup>
           <ModifierUneClasse data={data}>
             <DropdownMenuLabel className="cursor-pointer hover:text-black hover:rounded-sm hover:bg-secondeColor">
@@ -34,6 +28,7 @@ export const DropdownMenuItemSelect = ({ children, data }: DropdownMenuItemSelec
               <span>Supprimer</span>
             </DropdownMenuLabel>
           </SupprimerUneClasse>
+
           <ArchiveUneClasse classe_id={data.id}>
             <DropdownMenuLabel className="cursor-pointer hover:text-black hover:rounded-sm hover:bg-secondeColor">
               <span>Archiver</span>
@@ -44,3 +39,5 @@ export const DropdownMenuItemSelect = ({ children, data }: DropdownMenuItemSelec
     </DropdownMenu>
   );
 };
+
+export default DropdownMenuItemSelect;
