9bff0f9198fa2600f631114217b6718cd66b2280
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:33:11 +0100

    improve

--- app/@teacher/[etab_id]/(teacherdashboard)/_components/sidebar.tsx
+++ app/@teacher/[etab_id]/(teacherdashboard)/_components/sidebar.tsx
@@ -2,46 +2,46 @@ import { cn } from '@/lib/utils';
 import Logo from '../../../../../components/shared-components/logo';
 import { SidebarRoutes } from '../../../../../components/shared-components/sidebar-routes';
 
-const guestRoutes = [
-  {
-    Clickedicon: '/dashboardicon.svg',
-    Defaulticon: '/defaultdashboardicon.svg',
-    label: 'Tableau de bord',
-    href: '/',
-  },
-  {
-    Clickedicon: '/classesicon.svg',
-    Defaulticon: '/defaultclassesicon.svg',
-    label: 'Classes',
-    href: '/classes',
-  },
-  {
-    Clickedicon: '/examensicon.svg',
-    Defaulticon: '/defaultexamensicon.svg',
-    label: 'Examens',
-    href: '/examens',
-  },
-  {
-    Clickedicon: '/bulletinsicon.svg',
-    Defaulticon: '/defaultbulletinsicon.svg',
-    label: 'Bulletins',
-    href: '/bulletins',
-  },
-  {
-    Clickedicon: '/calendriericon.svg',
-    Defaulticon: '/defaultcalendriericon.svg',
-    label: 'Calendrier',
-    href: '/calendrier',
-  },
-  {
-    Clickedicon: '/archiveicon.svg',
-    Defaulticon: '/defaultarchiveicon.svg',
-    label: 'Archive',
-    href: '/archive/classes',
-  },
-];
-
 const Sidebar = ({ isOpen }: { isOpen: boolean }) => {
+  const guestRoutes = [
+    {
+      Clickedicon: '/dashboardicon.svg',
+      Defaulticon: '/defaultdashboardicon.svg',
+      label: 'Tableau de bord',
+      href: '/',
+    },
+    {
+      Clickedicon: '/classesicon.svg',
+      Defaulticon: '/defaultclassesicon.svg',
+      label: 'Classes',
+      href: '/classes',
+    },
+    {
+      Clickedicon: '/examensicon.svg',
+      Defaulticon: '/defaultexamensicon.svg',
+      label: 'Examens',
+      href: '/examens',
+    },
+    {
+      Clickedicon: '/bulletinsicon.svg',
+      Defaulticon: '/defaultbulletinsicon.svg',
+      label: 'Bulletins',
+      href: '/bulletins',
+    },
+    {
+      Clickedicon: '/calendriericon.svg',
+      Defaulticon: '/defaultcalendriericon.svg',
+      label: 'Calendrier',
+      href: '/calendrier',
+    },
+    {
+      Clickedicon: '/archiveicon.svg',
+      Defaulticon: '/defaultarchiveicon.svg',
+      label: 'Archive',
+      href: '/archive/classes',
+    },
+  ];
+
   return (
     <div
       className={cn(
@@ -49,15 +49,4 @@ const Sidebar = ({ isOpen }: { isOpen: boolean }) => {
         isOpen && ' bg-mainGreen'
       )}
     >
-      <div className={cn('p-4 border-none bg-silver', isOpen && 'bg-mainGreen p-5   text-sky-700')}>
-        <Logo />
-      </div>
-
-      <div className="flex flex-col h-full p-1 font-normal text-white ">
-        <SidebarRoutes guestRoutes={guestRoutes} />
-      </div>
-    </div>
-  );
-};
-
-export default Sidebar;
+      <div className={cn('p-4 border-none bg-silver', isOpen && 'bg-mainGreen p-
