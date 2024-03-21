bc3211b34884ae5b360321b38d20878f88518953
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:32:58 +0100

    improve

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,11 +1,22 @@
 import Image from "next/image";
 
 function Loading() {
-    return (
-      <div className="w-full h-[100vh] flex items-center justify-center">
-        <Image src="/loading.svg" alt="bankicon" width={400} height={400} />
+  return (
+    <div className="w-full h-[100vh] flex items-center justify-center">
+      <div className="text-center">
+        <p className="text-2xl font-semibold">Loading...</p>
+        <Image
+          src="/loading.svg"
+          alt="Bank Icon"
+          width={400}
+          height={400}
+          viewBox="0 0 400 400"
+          layout="intrinsic"
+          className="mx-auto mt-8"
+        />
       </div>
-    );
+    </div>
+  );
 }
 
-export default Loading
+export default Loading;
