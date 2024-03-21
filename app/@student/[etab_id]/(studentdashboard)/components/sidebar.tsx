1c5d4fb7dd9aec85f0cf2b55806f745c1b9f4420
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:32:53 +0100

    improve

--- app/@student/[etab_id]/(studentdashboard)/components/sidebar.tsx
+++ app/@student/[etab_id]/(studentdashboard)/components/sidebar.tsx
@@ -1,48 +1,40 @@
-import { cn } from '@/lib/utils';;
+import { cn } from '@/lib/utils';
 import { SidebarRoutes } from '../../../../../components/shared-components/sidebar-routes';
 import Logo from '@/components/shared-components/logo';
 
-
-
 const guestRoutes = [
   {
-    Clickedicon: '/dashboardicon.svg',
-    Defaulticon: '/defaultdashboardicon.svg',
     label: 'Tableau de bord',
     href: '/',
+    icon: {
+      clicked: '/dashboardicon.svg',
+      default: '/defaultdashboardicon.svg',
+    },
   },
-
   {
-    Clickedicon: '/bulletinsicon.svg',
-    Defaulticon: '/defaultbulletinsicon.svg',
     label: 'Mes résultats',
     href: '/results',
+    icon: {
+      clicked: '/bulletinsicon.svg',
+      default: '/defaultbulletinsicon.svg',
+    },
   },
   {
-    Clickedicon: '/calendriericon.svg',
-    Defaulticon: '/defaultcalendriericon.svg',
     label: 'Calendrier',
     href: '/calendrier',
+    icon: {
+      clicked: '/calendriericon.svg',
+      default: '/defaultcalendriericon.svg',
+    },
   },
 ];
 
-const Sidebar = ({ isOpen }: { isOpen: boolean }) => {
+const Sidebar = ({ isOpen }) => {
   return (
     <div
       className={cn(
-        'w-full h-full  border-r shadow-sm felx flex:col bg-mainGreen',
-        isOpen && ' bg-mainGreen'
+        'w-full h-full border-r shadow-sm flex flex-col',
+        isOpen ? 'bg-mainGreen' : 'bg-silver'
       )}
     >
-      <div className={cn('p-4 border-none bg-silver', isOpen && 'bg-mainGreen p-5   text-sky-700')}>
-        <Logo />
-      </div>
-
-      <div className="flex flex-col h-full p-1 font-normal text-white ">
-        <SidebarRoutes guestRoutes={guestRoutes} />
-      </div>
-    </div>
-  );
-};
-
-export default Sidebar;
+      <div className={cn('p-4 border-none', isOpen && 'bg-
