04acac0684b6dafc68f4b2e705d68505f1f80ada
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:32:48 +0100

    improve

--- actions/auth/registerInvitedStudent.ts
+++ actions/auth/registerInvitedStudent.ts
@@ -5,28 +5,30 @@ import { AuthError } from 'next-auth';
 import { db } from '@/lib/db';
 import bcryptjs from 'bcryptjs';
 import { sendVerificationEmail } from '@/lib/mail';
-export const RegisterInvitedStudent = async (values: z.infer<typeof InvitSchema>, code: number) => {
+
+export const registerInvitedStudent = async (values: z.infer<typeof InvitSchema>, code: number) => {
   const validatedFields = InvitSchema.safeParse(values);
 
   if (!validatedFields.success)
     return { error: 'Adresse e-mail ou mot de passe incorrect. Veuillez réessayer.' };
 
-  const { email, password } = validatedFields?.data;
+  const { email, password } = validatedFields.data;
 
   const hashedPassword = await bcryptjs.hash(password, 10);
+
   try {
-    await db.user
-      .update({
-        where: { email },
-        data: {
-          password: hashedPassword,
-        },
-      })
-      .then(async (data :any) => {
-        await sendVerificationEmail(email, code);
-      });
+    await db.user.update({
+      where: { email },
+      data: {
+        password: hashedPassword,
+      },
+    });
+
+    await sendVerificationEmail(email, code);
+
     return { success: 'Un e-mail a été envoyé ! Veuillez vérifier votre compte.' };
-  } catch (error ) {
-    throw error;
+  } catch (error) {
+    console.error(error);
+    throw new AuthError('Une erreur est survenue lors de l\'enregistrement de l\'étudiant invité.');
   }
 };
