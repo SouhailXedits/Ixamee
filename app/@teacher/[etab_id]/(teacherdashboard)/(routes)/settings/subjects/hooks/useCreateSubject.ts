92b14d5b614b88a04c032dbae1cc85062701e216
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:33:09 +0100

    improve

--- app/@teacher/[etab_id]/(teacherdashboard)/(routes)/settings/subjects/hooks/useCreateSubject.ts
+++ app/@teacher/[etab_id]/(teacherdashboard)/(routes)/settings/subjects/hooks/useCreateSubject.ts
@@ -1,18 +1,17 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { UseMutationResult, UseQueryClient } from '@tanstack/react-query';
 import { toast } from 'react-hot-toast';
 import { SubjectInputProps } from '@/types/subjects/subjectTypes';
 import { createSubject as createSubjectApi } from '@/actions/subjects';
 
-export function useCreateSubject() {
-  const queryClient = useQueryClient();
-  const { mutate: createSubject, isPending } = useMutation({
+export function useCreateSubject(queryClient: UseQueryClient) {
+  const { mutate: createSubject, isPending } = useMutation<void, Error, SubjectInputProps>({
     mutationFn: (data: SubjectInputProps) => createSubjectApi(data),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['subjects'] });
-      toast.success('Matière creé avec succès.');
+      toast.success('Matière créée avec succès.');
     },
     onError: (err) => {
-      toast.error('Une erreur est survenue lors de la création de la matière.');
+      toast.error('Une erreur est survenue lors de la création de la matière : ' + err.message);
     },
     retry: false,
   });
