a0024963d1929dab24e755d4db0e28fa4a105e47
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:33:02 +0100

    improve

--- app/providers/confetti-provider.tsx
+++ app/providers/confetti-provider.tsx
@@ -3,21 +3,22 @@
 import { useConfettiStore } from '@/store/use-confetti-store';
 import ReactConfetti from 'react-confetti';
 
-
-
 export const ConfettiProvider = () => {
-  const confetti = useConfettiStore();
-  console.log(confetti);
-  if (!confetti.isOpen) return null;
+  const { isOpen, onClose } = useConfettiStore();
+
+  // Only render the confetti if isOpen is true
+  if (!isOpen) return null;
+
+  // Use a document-level z-index to ensure the confetti appears on top of all elements
+  const zIndex = 2147483647; // Highest possible value for z-index
 
   return (
     <ReactConfetti
       className="pointer-events-none z-[100]"
+      style={{ zIndex }} // Set the z-index style property
       numberOfPieces={500}
       recycle={false}
-      onConfettiComplete={() => {
-        confetti.onClose();
-      }}
+      onConfettiComplete={onClose}
     />
   );
 };
