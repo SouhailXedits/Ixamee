4cf1c6622e50642b58a770fc6591ce9128497f2a
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:33:13 +0100

    improve

--- components/shared-components/navbar-notfication.tsx
+++ components/shared-components/navbar-notfication.tsx
@@ -1,11 +1,24 @@
 import Image from 'next/image';
-
 import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/components/ui/hover-card';
 
 const NavbarNotification = () => {
+  const notifications = [
+    {
+      id: 1,
+      message: 'Nouha Khlif a accepté votre invitation.',
+    },
+    {
+      id: 2,
+      message: 'Maher Laadheri a accepté votre invitation.',
+    },
+    {
+      id: 3,
+      message: 'Nouha Khlif a accepté votre invitation.',
+    },
+  ];
+
   return (
     <HoverCard>
-    console.log(`<HoverCard>
       <HoverCardTrigger asChild>
         <div className="relative flex">
           <Image
@@ -15,11 +28,11 @@ const NavbarNotification = () => {
             height={37}
             className="cursor-pointer"
           />
-          <div className="absolute -top-1 -right-1 w-[15px] h-[15px] p-2.5 bg-[#FEE4E2] rounded-[53px] justify-center items-center gap-2.5 inline-flex">
-            <div className="animate-ping text-center text-[#F04438] text-[10px] font-bold font-mono ">
-              1
+          {notifications.length > 0 && (
+            <div className="absolute -top-1 -right-1 w-[15px] h-[15px] p-2.5 bg-[#FEE4E2] rounded-[53px] justify-center items-center gap-2.5 inline-flex">
+              <div className="animate-ping text-center text-[#F04438] text-[10px] font-bold font-mono ">{notifications.length}</div>
             </div>
-          </div>
+          )}
         </div>
       </HoverCardTrigger>
 
@@ -29,118 +42,21 @@ const NavbarNotification = () => {
             Notifications
           </div>
 
-          {/* acune notification  */}
-
-          {/* <div className="w-full text-start text-[#959595]   pt-6 font-thin ">
-        Vous n’avez aucune nouvelle notification.
-        </div> */}
-
-          {/*  notification */}
-
-          <div className="w-full text-start text-[#959595]    font-thin   cursor-pointer duration-300 flex flex-col gap-2  overflow-auto max-h-[170px] ">
-            <span className="p-2 rounded-lg hover:bg-secondeColor ">
-              Nouha Khlif a accepté votre invitation.
-            </span>
-            <span className="p-2 rounded-lg hover:bg-secondeColor ">
-              Maher Laadheri a accepté votre invitation.
-            </span>
-
-            <span className="p-2 rounded-lg hover:bg-secondeColor ">
-              Nouha Khlif a accepté votre invitation.
-            </span>
-          </div>
-        </div>
-      </HoverCardContent>
-    </HoverCard> :`, <HoverCard>
-      <HoverCardTrigger asChild>
-        <div className="relative flex">
-          <Image
-            src="/navbarnotification.svg"
-            alt="notification"
-            width={37}
-            height={37}
-            className="cursor-pointer"
-          />
-          <div className="absolute -top-1 -right-1 w-[15px] h-[15px] p-2.5 bg-[#FEE4E2] rounded-[53px] justify-center items-center gap-2.5 inline-flex">
-            <div className="animate-ping text-center text-[#F04438] text-[10px] font-bold font-mono ">
-              1
+          {notifications.length === 0 && (
+            <div className="w-full text-start text-[#959595]   pt-6 font-thin ">
+              Vous n’avez aucune nouvelle notification.
             </div>
-          </div>
-        </div>
-      </HoverCardTrigger>
+          )}
 
-      <HoverCardContent className="w-90">
-        <div className="flex flex-col justify-between w-full gap-3 ">
-          <div className=" text-[#1B8392] text-xl font-medium  border-b border-[#1B8392] pb-2">
-            Notifications
-          </div>
-
-          {/* acune notification  */}
-
-          {/* <div className="w-full text-start text-[#959595]   pt-6 font-thin ">
-        Vous n’avez aucune nouvelle notification.
-        </div> */}
-
-          {/*  notification */}
-
-          <div className="w-full text-start text-[#959595]    font-thin   cursor-pointer duration-300 flex flex-col gap-2  overflow-auto max-h-[170px] ">
-            <span className="p-2 rounded-lg hover:bg-secondeColor ">
-              Nouha Khlif a accepté votre invitation.
-            </span>
-            <span className="p-2 rounded-lg hover:bg-secondeColor ">
-              Maher Laadheri a accepté votre invitation.
-            </span>
-
-            <span className="p-2 rounded-lg hover:bg-secondeColor ">
-              Nouha Khlif a accepté votre invitation.
-            </span>
-          </div>
-        </div>
-      </HoverCardContent>
-    </HoverCard>);
-      <HoverCardTrigger asChild>
-        <div className="relative flex">
-          <Image
-            src="/navbarnotification.svg"
-            alt="notification"
-            width={37}
-            height={37}
-            className="cursor-pointer"
-          />
-          <div className="absolute -top-1 -right-1 w-[15px] h-[15px] p-2.5 bg-[#FEE4E2] rounded-[53px] justify-center items-center gap-2.5 inline-flex">
-            <div className="animate-ping text-center text-[#F04438] text-[10px] font-bold font-mono ">
-              1
+          {notifications.length > 0 && (
+            <div className="w-full text-start text-[#959595]    font-thin   cursor-pointer duration-300 flex flex-col gap-2  overflow-auto max-h-[170px] ">
+              {notifications.map((notification) => (
+                <span key={notification.id} className="p-2 rounded-lg hover:bg-secondeColor ">
+                  {notification.message}
+                </span>
+              ))}
             </div>
-          </div>
-        </div>
-      </HoverCardTrigger>
-
-      <HoverCardContent className="w-90">
-        <div className="flex flex-col justify-between w-full gap-3 ">
-          <div className=" text-[#1B8392] text-xl font-medium  border-b border-[#1B8392] pb-2">
-            Notifications
-          </div>
-
-          {/* acune notification  */}
-
-          {/* <div className="w-full text-start text-[#959595]   pt-6 font-thin ">
-        Vous n’avez aucune nouvelle notification.
-        </div> */}
-
-          {/*  notification */}
-
-          <div className="w-full text-start text-[#959595]    font-thin   cursor-pointer duration-300 flex flex-col gap-2  overflow-auto max-h-[170px] ">
-            <span className="p-2 rounded-lg hover:bg-secondeColor ">
-              Nouha Khlif a accepté votre invitation.
-            </span>
-            <span className="p-2 rounded-lg hover:bg-secondeColor ">
-              Maher Laadheri a accepté votre invitation.
-            </span>
-
-            <span className="p-2 rounded-lg hover:bg-secondeColor ">
-              Nouha Khlif a accepté votre invitation.
-            </span>
-          </div>
+          )}
         </div>
       </HoverCardContent>
     </HoverCard>
