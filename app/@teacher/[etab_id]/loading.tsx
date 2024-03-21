6709a453f82f9ce736e43eb9d250df2cdf27bde0
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:32:58 +0100

    improve

--- app/@teacher/[etab_id]/loading.tsx
+++ app/@teacher/[etab_id]/loading.tsx
@@ -1,9 +1,14 @@
 import Image from 'next/image';
 
-function Loading() {
+function Loading({ imageWidth = 400, imageHeight = 400, altText = 'Loading indicator' }) {
   return (
-    <div className="w-full h-[100vh] flex items-center justify-center">
-      <Image src="/loading.svg" alt="bankicon" width={400} height={400} />
+    <div className="w-full h-screen flex items-center justify-center">
+      <Image
+        src="/loading.svg"
+        alt={altText}
+        width={imageWidth}
+        height={imageHeight}
+      />
     </div>
   );
 }
