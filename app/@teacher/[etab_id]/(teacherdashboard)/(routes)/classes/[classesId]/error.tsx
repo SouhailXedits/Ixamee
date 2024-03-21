8b68ac00f7f5135507e11c872c2841d5e13ac15b
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:32:55 +0100

    improve

--- app/@teacher/[etab_id]/(teacherdashboard)/(routes)/classes/[classesId]/error.tsx
+++ app/@teacher/[etab_id]/(teacherdashboard)/(routes)/classes/[classesId]/error.tsx
@@ -2,13 +2,18 @@
 import Image from 'next/image';
 import React from 'react';
 
-function Errorr() {
+const ErrorPage = () => {
   return (
     <div>
-      <Image src={'./notFoundSvg.svg'} width={300} height={300} alt="not found page" />
-      Error
+      <Image
+        src="/notFoundSvg.svg" // remove the dot before the slash for correct path
+        alt="not found page"
+        width={300}
+        height={300}
+      />
+      <h1>Error</h1> {/* add heading tag for better accessibility */}
     </div>
   );
-}
+};
 
-export default Errorr;
+export default ErrorPage;
