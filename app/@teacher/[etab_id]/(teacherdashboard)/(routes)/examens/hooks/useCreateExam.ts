7101c1280286e5a69ca3589feea6bf8a386c3b34
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:33:02 +0100

    improve

--- app/@teacher/[etab_id]/(teacherdashboard)/(routes)/examens/hooks/useCreateExam.ts
+++ app/@teacher/[etab_id]/(teacherdashboard)/(routes)/examens/hooks/useCreateExam.ts
@@ -1,33 +1,43 @@
 'use client';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-
 import { toast } from 'react-hot-toast';
 import { createExamm as createExammApi } from '@/actions/examens';
-import { useParams, usePathname, useRouter } from 'next/navigation';
+import { useRouter } from 'next/router';
+import { ParsedUrlQuery } from 'querystring';
 
 export function useCreateExam() {
   const queryClient = useQueryClient();
-  const params = useParams();
-  const pathName = usePathname();
   const router = useRouter();
-  const { mutate: creatExam, isPending } = useMutation({
-    mutationFn: ({ data, user_id }: { data: any; user_id: string }) =>
-      createExammApi(data, user_id),
-    onSuccess: (data: any) => {
-      queryClient.invalidateQueries({ queryKey: ['examens'] });
-      queryClient.invalidateQueries({ queryKey: ['dashExamCount'] });
-      toast.success('Exam ajoute avec succès.');
-      if (pathName.includes('/examens')) {
-        router.push(pathName + '/' + data.id);
-      } else {
-        router.push(params.etab_id + '/examens/' + data.id);
-      }
-    },
-    onError: (err) => {
-      toast.error('There was an error creating the Exam');
-    },
-    retry: false,
-  });
 
-  return { creatExam, isPending };
+  const { mutate: createExam, isPending } = useMutation<
+    any,
+    Error,
+    { data: any; user_id: string },
+    any
+  >(
+    ({ data, user_id }) => createExammApi(data, user_id),
+    {
+      onSuccess: (data) => {
+        const queryKeys = ['examens', 'dashExamCount'];
+        queryClient.invalidateQueries(queryKeys);
+        toast.success('Exam added successfully.');
+
+        const { etab_id, ...restParams } = router.query as ParsedUrlQuery;
+        router.push(
+          {
+            pathname: pathName + '/' + data.id,
+            query: restParams,
+          },
+          undefined,
+          { shallow: true }
+        );
+      },
+      onError: (err) => {
+        toast.error('Error creating the Exam');
+      },
+      retry: false,
+    }
+  );
+
+  return { createExam, isPending };
 }
