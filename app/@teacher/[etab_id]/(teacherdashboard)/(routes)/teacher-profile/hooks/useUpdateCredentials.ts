4305ae00e01da1cd128eaba752312eac0cdf2c12
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:33:04 +0100

    improve

--- app/@teacher/[etab_id]/(teacherdashboard)/(routes)/teacher-profile/hooks/useUpdateCredentials.ts
+++ app/@teacher/[etab_id]/(teacherdashboard)/(routes)/teacher-profile/hooks/useUpdateCredentials.ts
@@ -1,28 +1,35 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-
+import { UseMutationResult, UseQueryClient } from '@tanstack/react-query';
 import { toast } from 'react-hot-toast';
 import { updateTeacherCredentials } from '@/actions/profile/updateUserCredentials';
 
-export function useUpdateCredentials() {
-  const queryClient = useQueryClient();
-  const { mutate: updateUser, isPending } = useMutation({
-    mutationFn: (values: any) => updateTeacherCredentials(values),
-    onSuccess: (data) => {
-      if (data?.success) {
-        queryClient.invalidateQueries({ queryKey: ['user'] });
-        queryClient.invalidateQueries({ queryKey: ['teacherSubjects'] });
-        queryClient.invalidateQueries({ queryKey: ['AllEstabOfUser'] });
-        toast.success(data.success + '');
-      }
-      if (data?.error) {
-        toast.error(data.error);
-      }
-    },
-    onError: (err) => {
-      console.error(err);
-      toast.error('Une erreur est survenue ressayer une autre fois');
-    },
-    retry: false,
-  });
+export interface UpdateCredentialsProps {
+  onSuccess: () => void;
+  onError: () => void;
+}
+
+export function useUpdateCredentials(queryClient: UseQueryClient) {
+  const { mutate: updateUser, isPending } = useMutation<
+    any,
+    Error,
+    any,
+    UpdateCredentialsProps
+  >(
+    (values: any) => updateTeacherCredentials(values),
+    {
+      onSuccess: ({ success }) => {
+        if (success) {
+          queryClient.invalidateQueries({ queryKey: ['user'] });
+          queryClient.invalidateQueries({ queryKey: ['teacherSubjects'] });
+          queryClient.invalidateQueries({ queryKey: ['AllEstabOfUser'] });
+          toast.success(success);
+        }
+      },
+      onError: () => {
+        toast.error('Une erreur est survenue, réessayez une autre fois');
+      },
+      retry: false,
+    }
+  );
+
   return { updateUser, isPending };
 }
