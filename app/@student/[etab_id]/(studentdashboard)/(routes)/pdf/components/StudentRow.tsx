55c6217a8f592a19bdcafe015b7cabf0db703e4c
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:32:52 +0100

    improve

--- app/@student/[etab_id]/(studentdashboard)/(routes)/pdf/components/StudentRow.tsx
+++ app/@student/[etab_id]/(studentdashboard)/(routes)/pdf/components/StudentRow.tsx
@@ -1,17 +1,33 @@
-function StudentRow({data}: any) {
+import React from 'react';
 
-
-    return (
-      <tr>
-        <td className="pb-4 border border-black/50 ">{data.name}</td>
-        <td className="pb-4 border border-black/50">{data.dc1.note}</td>
-        <td className="pb-4 border border-black/50">{data.dc1.rang}</td>
-        <td className="pb-4 border border-black/50">{data.dc2.note}</td>
-        <td className="pb-4 border border-black/50">{data.dc2.rang}</td>
-        <td className="pb-4 border border-black/50">{data.ds1.note}</td>
-        <td className="pb-4 border border-black/50">{data.ds1.rang}</td>
-      </tr>
-    );
+interface StudentDataable {
+  name: string;
+  dc1: Data;
+  dc2: Data;
+  ds1: Data;
 }
 
-export default StudentRow
+interface Data {
+  note: string;
+  rang: string;
+}
+
+interface StudentRowProps {
+  data: StudentDataable;
+}
+
+const StudentRow: React.FC<StudentRowProps> = ({ data }) => {
+  return (
+    <tr>
+      <td className="pb-4 border border-black/50">{data.name}</td>
+      <td className="pb-4 border border-black/50">{data.dc1.note}</td>
+      <td className="pb-4 border border-black/50">{data.dc1.rang}</td>
+      <td className="pb-4 border border-black/50">{data.dc2.note}</td>
+      <td className="pb-4 border border-black/50">{data.dc2.rang}</td>
+      <td className="pb-4 border border-black/50">{data.ds1.note}</td>
+      <td className="pb-4 border border-black/50">{data.ds1.rang}</td>
+    </tr>
+  );
+};
+
+export default StudentRow;
