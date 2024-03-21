d7fa73d971a5a44274f2e120e244100514578656
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:32:49 +0100

    improve

--- app/@student/[etab_id]/(studentdashboard)/(routes)/(root)/_components/classes-cards.tsx
+++ app/@student/[etab_id]/(studentdashboard)/(routes)/(root)/_components/classes-cards.tsx
@@ -2,13 +2,29 @@ import Image from 'next/image';
 import SubjectCard from '../../results/components/SubjectCard';
 import { Skeleton } from '@/components/ui/skeleton';
 
-const ClassesCards = ({ subjects, isPending }: any) => {
+type ClassesCardsProps = {
+  subjects?: Array<{
+    id: string;
+    classe_subject: {
+      id: string;
+    };
+  }>;
+  isPending: boolean;
+};
+
+const ClassesCards = ({ subjects, isPending }: ClassesCardsProps) => {
   return (
     <div className="flex flex-wrap h-full gap-8 ">
-      {isPending && <Skeleton className=" w-[350px] rounded-[20px] h-[200px] " />}
-      {subjects?.map((subject: any) => (
-        <SubjectCard key={subject.id} subject={subject} etab_id={subject.classe_subject.id} />
-      ))}
+      {isPending && <Skeleton key="skeleton" className=" w-[350px] rounded-[20px] h-[200px] " />}
+      {subjects?.map((subject) =>
+        subject ? (
+          <SubjectCard
+            key={subject.id}
+            subject={subject}
+            etab_id={subject.classe_subject.id}
+          />
+        ) : null
+      )}
     </div>
   );
 };
