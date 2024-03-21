3bc2f0a0d80ac974c6c8b00b29ce7fe6db9f16d4
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:33:06 +0100

    improve

--- app/@teacher/[etab_id]/(teacherdashboard)/(routes)/settings/teachers/page.tsx
+++ app/@teacher/[etab_id]/(teacherdashboard)/(routes)/settings/teachers/page.tsx
@@ -5,25 +5,20 @@ import { AddTeacher } from '@/app/@teacher/[etab_id]/(teacherdashboard)/(routes)
 import { useQuery } from '@tanstack/react-query';
 import { getAllAdminTeachers } from '@/actions/teachers';
 import { useState } from 'react';
+import Link from 'next/link';
 
 const Teacher = () => {
   const [searchQuery, setSearchQuery] = useState('');
 
   const {
     data: teachers,
+    isLoading,
+    isError,
     error,
-    isPending,
   } = useQuery<any>({
     queryKey: ['teachers-admins', searchQuery],
     queryFn: async () => await getAllAdminTeachers(searchQuery),
   });
-  // const {
-  //   data: seachData,
-  //   isPending : isSearchPending,
-  // } = useQuery<any>({
-  //   queryKey: ['teachers-admins-seach'],
-  //   queryFn: async () => await getAllSearchQuery(searchQuery, table),
-  // });
 
   const data = teachers?.data;
 
@@ -33,22 +28,17 @@ const Teacher = () => {
         <div className="flex flex-col gap-4">
           <div className="text-[#1B8392] text-2xl font-semibold ">Paramètres</div>
           <div className="flex items-center text-[#727272]">
-            {/* <Image src="/arrowleft.svg" alt="icons" width={20} height={20} /> */}
-
-            {/* <Link href={'/settings'} className="cursor-pointer">
+            <Link href={'/settings'} className="cursor-pointer">
               Classes
-            </Link> */}
-            <Image src="/arrowleft.svg" alt="icons" width={20} height={20} />
-
+            </Link>
+            <Image src="/arrowleft.svg" alt="arrow left icon" width={20} height={20} />
             <span className="cursor-pointer">Admins</span>
           </div>
         </div>
 
         <div className="flex gap-3 pt-4 h-14 cursor-pointe ">
-          {/* <SearchModal field="enseignant" table="user"> */}
           <div className="flex items-center p-2 border rounded-lg cursor-pointer border-[#99C6D3] gap-3 hover:opacity-80 ">
-            <Image src="/scoop.svg" alt="icons" width={20} height={20} />
-
+            <Image src="/scoop.svg" alt="search icon" width={20} height={20} />
             <input
               type="text"
               placeholder="Recherche"
@@ -56,7 +46,6 @@ const Teacher = () => {
               className=" w-24 bg-transparent outline-none border-none  text-sm font-semibold  leading-tight placeholder-[#99C6D3]"
             />
           </div>
-          {/* </SearchModal> */}
           <AddTeacher>
             <button
               name="btn"
@@ -68,9 +57,13 @@ const Teacher = () => {
         </div>
       </nav>
 
-      <div>
-        <TeacherAdminsList data={data} isPending={isPending} />
-      </div>
+      {isLoading && <div>Loading...</div>}
+      {isError && <div>Error: {error.message}</div>}
+      {data && (
+        <div>
+          <TeacherAdminsList data={data} isPending={isLoading} />
+        </div>
+      )}
     </main>
   );
 };
