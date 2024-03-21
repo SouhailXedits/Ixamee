517ec1c2071349ca1baddc0c69162c6614b8685b
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:32:41 +0100

    improve

--- actions/auth/reset.ts
+++ actions/auth/reset.ts
@@ -1,23 +1,30 @@
 'use server';
 import * as z from 'zod';
-import { ResetSchema } from './schemas';
 import { getUserByEmail } from '@/data/user';
 import { sendPasswordResetEmail, sendVerificationEmail } from '@/lib/mail';
 import { generatePasswordResetToken } from '@/lib/tokens';
 
-export const reset = async (values: z.infer<typeof ResetSchema>, code: number) => {
+const ResetSchema = z.object({
+  email: z.string().email(),
+});
+
+export const reset = async (req: Request) => {
+  const values = await req.json();
   const validatedFields = ResetSchema.safeParse(values);
+
   if (!validatedFields.success) {
-    return { error: 'E-mail invalid' };
+    return new Response('E-mail invalid', { status: 400 });
   }
 
   const { email } = validatedFields.data;
 
   const existingUser = await getUserByEmail(email);
+
   if (!existingUser) {
-    return { error: "Nous n'avons pas réussi à trouver votre e-mail" };
+    return new Response("Nous n'avons pas réussi à trouver votre e-mail", { status: 404 });
   }
+
   const passwordResetToken = await generatePasswordResetToken(email);
-  await sendPasswordResetEmail(existingUser.email, passwordResetToken.token, code);
-  return { success: 'Nous vous avons envoyé un email pour réinitialiser votre mot de passe' };
+  await sendPasswordResetEmail(existingUser.email, passwordResetToken.token);
+  return new Response('Nous vous avons envoyé un email pour réinitialiser votre mot de passe', { status: 200 });
 };
