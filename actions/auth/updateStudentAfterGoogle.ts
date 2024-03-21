5e1ada44f7022c4008f6c4aa4757e4a3fd6aad88
Souhail Brahmi <144920931+SouhailXedits@users.noreply.github.com>
2024-03-21 13:32:47 +0100

    improve

--- actions/auth/updateStudentAfterGoogle.ts
+++ actions/auth/updateStudentAfterGoogle.ts
@@ -4,9 +4,11 @@ import { EtudiantAfterSchema } from '@/actions/auth/schemas';
 import { db } from '@/lib/db';
 import { getUserByEmail } from '@/data/user';
 
+type UserRole = 'ADMIN' | 'STUDENT' | 'TEACHER';
+
 export const updateStudentAfterGoogle = async (values: z.infer<typeof EtudiantAfterSchema>) => {
   const validatedFields = EtudiantAfterSchema.safeParse(values);
-  const existingUser = values?.email ? await getUserByEmail(values?.email) : undefined;
+  const existingUser = validatedFields.success && values?.email ? await getUserByEmail(values?.email) : undefined;
 
   if (!validatedFields.success) {
     return { error: 'Veuillez renseigner tous les champs.' };
@@ -15,20 +17,16 @@ export const updateStudentAfterGoogle = async (values: z.infer<typeof EtudiantAf
   if (!validatedFields.success || !existingUser) {
     return { error: "Une erreur s'est produite. Veuillez réessayer." };
   }
-  const { government, etablissement: establishment } = validatedFields.data;
-  try {
-    enum UserRole {
-      ADMIN = 'ADMIN',
-      STUDENT = 'STUDENT',
-      TEACHER = 'TEACHER',
-    }
-    const mappedRole =
-      existingUser?.role || values?.role === 'TEACHER'
-        ? UserRole.TEACHER
-        : existingUser?.role || values?.role === 'STUDENT'
-        ? UserRole.STUDENT
-        : UserRole.ADMIN;
 
+  const { government, etablissement: establishment } = validatedFields.data;
+
+  const mappedRole: UserRole = (existingUser.role || values.role) === 'TEACHER'
+    ? 'TEACHER'
+    : (existingUser.role || values.role) === 'STUDENT'
+    ? 'STUDENT'
+    : 'ADMIN';
+
+  try {
     await db.user.update({
       where: { id: existingUser?.id },
       data: {
